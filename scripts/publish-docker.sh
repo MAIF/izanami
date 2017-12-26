fdfd8ac824a85b489d007ecfd9fba9b6f0ee4844
adelegue <adelegue@hotmail.com>
2017-12-26 15:47:33 +0100

    Publish

--- scripts/publish-docker.sh
+++ scripts/publish-docker.sh
@@ -1,2 +1,7 @@
+#!/usr/bin/env bash
+
+echo 'Publishing docker package'
+
 docker tag izanami maif-docker-docker.bintray.io/izanami
+
 docker push maif-docker-docker.bintray.io/izanami
--- scripts/publish-npm.sh
+++ scripts/publish-npm.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+
+echo 'Publishing npm packages'
+
+LOCATION=`pwd`
+
+cp ${LOCATION}/.npmrc ${LOCATION}/izanami-clients/react/
+
+cp ${LOCATION}/.npmrc ${LOCATION}/izanami-clients/node/
+
+cd ${LOCATION}/izanami-clients/react
+npm unpublish --force
+yarn publish --new-version ${PACKAGE_VERSION}
+
+cd ${LOCATION}/izanami-clients/node
+npm unpublish --force
+yarn publish --new-version ${PACKAGE_VERSION}
+
+cd ${LOCATION}
