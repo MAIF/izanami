6f64845af9417820606ec6d1c73eeb67c12e2061
adelegue <adelegue@hotmail.com>
2017-12-27 14:49:06 +0100

    CI

--- scripts/publish-npm.sh
+++ scripts/publish-npm.sh
@@ -4,6 +4,11 @@ echo 'Publishing npm packages'
 
 LOCATION=`pwd`
 
+echo "//registry.npmjs.org/:_password=${NPM_PASSWORD}" > ~/.npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_AUTH}" >> ~/.npmrc
+echo "//registry.npmjs.org/:username=adelegue" >> ~/.npmrc
+echo "//registry.npmjs.org/:email=aadelegue@gmail.com" >> ~/.npmrc
+
 cd ${LOCATION}/izanami-clients/react
 npm unpublish --force
 npm publish
