333d4ce50a82ecf71f544a739cff365572307fe6
adelegue <adelegue@hotmail.com>
2017-12-28 20:55:40 +0100

    Tests

--- izanami-clients/jvm/src/test/scala/izanami/features/SmartCacheFeatureClientSpec.scala
+++ izanami-clients/jvm/src/test/scala/izanami/features/SmartCacheFeatureClientSpec.scala
@@ -8,11 +8,7 @@ import akka.stream.scaladsl.Sink
 import akka.testkit.TestKit
 import com.github.tomakehurst.wiremock.client.WireMock._
 import izanami.FeatureEvent.FeatureUpdated
-import izanami.Strategy.{
-  CacheWithPollingStrategy,
-  CacheWithSseStrategy,
-  FetchWithCacheStrategy
-}
+import izanami.Strategy.{CacheWithPollingStrategy, CacheWithSseStrategy, FetchWithCacheStrategy}
 import izanami._
 import izanami.scaladsl.{Features, IzanamiClient}
 import org.scalatest.BeforeAndAfterAll
@@ -30,7 +26,7 @@ class SmartCacheFeatureClientSpec
     with FeatureServer
     with FeatureMockServer {
 
-  implicit val system = ActorSystem("test")
+  implicit val system       = ActorSystem("test")
   implicit val materializer = ActorMaterializer()
 
   import system.dispatcher
@@ -84,7 +80,8 @@ class SmartCacheFeatureClientSpec
           .withQueryParam("pattern", equalTo("*"))
           .withQueryParam("active", equalTo("true"))
           .withQueryParam("page", equalTo("1"))
-          .withQueryParam("pageSize", equalTo("200")))
+          .withQueryParam("pageSize", equalTo("200"))
+      )
       mock.resetRequests()
 
       features.isActive("test1") must be(true)
@@ -116,7 +113,8 @@ class SmartCacheFeatureClientSpec
           .withQueryParam("pattern", equalTo("*"))
           .withQueryParam("active", equalTo("true"))
           .withQueryParam("page", equalTo("1"))
-          .withQueryParam("pageSize", equalTo("200")))
+          .withQueryParam("pageSize", equalTo("200"))
+      )
       mock.resetRequests()
 
       featuresUpdated.featuresSeq must be(fallback ++ updatedFeatures)
@@ -136,7 +134,8 @@ class SmartCacheFeatureClientSpec
       featureClient.checkFeature("test2", context).futureValue must be(true)
       mock.verifyThat(
         postRequestedFor(urlPathEqualTo("/api/features/test2/check"))
-          .withRequestBody(equalTo(Json.stringify(context))))
+          .withRequestBody(equalTo(Json.stringify(context)))
+      )
 
     }
 
@@ -168,10 +167,8 @@ class SmartCacheFeatureClientSpec
         val events = strategy.featuresSource("*").take(1).runWith(Sink.seq)
 
         events.futureValue must be(
-          Seq(
-            FeatureUpdated("test1",
-                           DefaultFeature("test1", true),
-                           DefaultFeature("test1", false))))
+          Seq(FeatureUpdated("test1", DefaultFeature("test1", true), DefaultFeature("test1", false)))
+        )
         promise.future.futureValue must be(DefaultFeature("test1", true))
       }
     }
@@ -218,10 +215,7 @@ class SmartCacheFeatureClientSpec
         ctx.calls.size must be(1)
 
         // We update feature via sse
-        ctx.push(
-          FeatureUpdated("test1",
-                         DefaultFeature("test1", false),
-                         DefaultFeature("test1", true)))
+        ctx.push(FeatureUpdated("test1", DefaultFeature("test1", false), DefaultFeature("test1", true)))
 
         //We wait that the events arrive
         val featuresUpdated: Features = pattern
@@ -253,74 +247,71 @@ class SmartCacheFeatureClientSpec
 
       }
     }
-
-    "Features by pattern with date with sse" in {
-      runServer { ctx =>
-        import akka.pattern
-        // This feature should be active
-        val releaseDateFeature =
-          ReleaseDateFeature("test1",
-                             true,
-                             LocalDateTime.now().minusSeconds(50))
-        //Init
-        val initialFeatures = Seq(releaseDateFeature)
-        ctx.setValues(initialFeatures)
-
-        val strategy = IzanamiClient(
-          ClientConfig(ctx.host)
-        ).featureClient(
-          strategy = CacheWithSseStrategy(
-            patterns = Seq("*")
-          )
-        )
-
-        //Waiting for the client to start polling
-        val features: Features = pattern
-          .after(3.seconds, system.scheduler) {
-            strategy.features("*")
-          }
-          .futureValue
-
-        //Only one call for the first fetch
-        ctx.calls.size must be(1)
-        features.isActive("test1") must be(true)
-
-        // As the data are in cache for the pattern, this should not generate an http call
-        strategy.checkFeature("test1").futureValue must be(true)
-        ctx.calls.size must be(1)
-
-        // We update feature via sse => the date is in the past so the feature become inactive.
-        ctx.push(
-          FeatureUpdated("test1",
-                         ReleaseDateFeature("test1",
-                                            true,
-                                            LocalDateTime.now().plusSeconds(2)),
-                         releaseDateFeature))
-
-        //We wait that the events arrive
-        val featuresUpdated: Features = pattern
-          .after(1.second, system.scheduler) {
-            strategy.features("*")
-          }
-          .futureValue
-
-        //With SSE we should only have the first fetch
-        ctx.calls.size must be(1)
-
-        // The state of the feature should be calculated on the client side.
-        featuresUpdated.isActive("test1") must be(false)
-
-        // As the data are in cache for the pattern, this should not generate an http call
-        strategy.checkFeature("test1").futureValue must be(false)
-        ctx.calls.size must be(1)
-
-        Thread.sleep(1600)
-        strategy.checkFeature("test1").futureValue must be(true)
-        ctx.calls.size must be(1)
-        strategy.features("*", Json.obj("withContext" -> true)).futureValue
-        ctx.calls.size must be(2)
-      }
-    }
+//
+//    "Features by pattern with date with sse" in {
+//      runServer { ctx =>
+//        import akka.pattern
+//        // This feature should be active
+//        val releaseDateFeature =
+//          ReleaseDateFeature("test1", true, LocalDateTime.now().minusSeconds(50))
+//        //Init
+//        val initialFeatures = Seq(releaseDateFeature)
+//        ctx.setValues(initialFeatures)
+//
+//        val strategy = IzanamiClient(
+//          ClientConfig(ctx.host)
+//        ).featureClient(
+//          strategy = CacheWithSseStrategy(
+//            patterns = Seq("*")
+//          )
+//        )
+//
+//        //Waiting for the client to start polling
+//        val features: Features = pattern
+//          .after(3.seconds, system.scheduler) {
+//            strategy.features("*")
+//          }
+//          .futureValue
+//
+//        //Only one call for the first fetch
+//        ctx.calls.size must be(1)
+//        features.isActive("test1") must be(true)
+//
+//        // As the data are in cache for the pattern, this should not generate an http call
+//        strategy.checkFeature("test1").futureValue must be(true)
+//        ctx.calls.size must be(1)
+//
+//        // We update feature via sse => the date is in the past so the feature become inactive.
+//        ctx.push(
+//          FeatureUpdated("test1",
+//                         ReleaseDateFeature("test1", true, LocalDateTime.now().plusSeconds(2)),
+//                         releaseDateFeature)
+//        )
+//
+//        //We wait that the events arrive
+//        val featuresUpdated: Features = pattern
+//          .after(1.second, system.scheduler) {
+//            strategy.features("*")
+//          }
+//          .futureValue
+//
+//        //With SSE we should only have the first fetch
+//        ctx.calls.size must be(1)
+//
+//        // The state of the feature should be calculated on the client side.
+//        featuresUpdated.isActive("test1") must be(false)
+//
+//        // As the data are in cache for the pattern, this should not generate an http call
+//        strategy.checkFeature("test1").futureValue must be(false)
+//        ctx.calls.size must be(1)
+//
+//        Thread.sleep(1600)
+//        strategy.checkFeature("test1").futureValue must be(true)
+//        ctx.calls.size must be(1)
+//        strategy.features("*", Json.obj("withContext" -> true)).futureValue
+//        ctx.calls.size must be(2)
+//      }
+//    }
   }
 
 }
