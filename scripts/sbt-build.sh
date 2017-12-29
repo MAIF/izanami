6eb01682e035bd58c631157feee20a42455ce6a2
adelegue <adelegue@hotmail.com>
2017-12-29 10:22:44 +0100

    CI for release

--- scripts/sbt-build.sh
+++ scripts/sbt-build.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 
-sbt -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION "test"
-sbt -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION ";izanami-server/assembly;izanami-server/dist"
-sbt -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION ";izanami-server/docker:publish"
-echo "Uploading izanami.jar"
-curl -T ./izanami-server/target/scala-2.12/izanami.jar -u${BINTRAY_USER}:${BINTRAY_PASSWORD} -H 'X-Bintray-Publish: 1' -H 'X-Bintray-Override: 1' -H 'X-Bintray-Version: latest' -H 'X-Bintray-Package: izanami.jar' https://api.bintray.com/content/maif/binaries/izanami.jar/latest/izanami.jar
-curl -T ./izanami-server/target/universal/izanami.zip -u${BINTRAY_USER}:${BINTRAY_PASSWORD} -H 'X-Bintray-Publish: 1' -H 'X-Bintray-Override: 1' -H 'X-Bintray-Version: latest' -H 'X-Bintray-Package: izanami-dist' https://api.bintray.com/content/maif/binaries/izanami-dist/latest/izanami-dist.zip
 
 if [-z "$TRAVIS_TAG"];
 then
-    echo 'Not a tag, nothing to to do'
+    echo 'Not a tag, just run test'
+    sbt -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION "test"
 else
 echo "Tag ${TRAVIS_TAG}, Publishing client"
-    sbt -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION "project jvm" "publish"
+    sbt -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION ";test;publish"
     echo "Uploading izanami.jar"
     BINARIES_VERSION=`echo "${TRAVIS_TAG}" | cut -d "v" -f 2`
     curl -T ./izanami-server/target/scala-2.12/izanami.jar -u${BINTRAY_USER}:${BINTRAY_PASSWORD} -H 'X-Bintray-Publish: 1' -H 'X-Bintray-Override: 1' -H 'X-Bintray-Version: ${BINARIES_VERSION}' -H 'X-Bintray-Package: izanami.jar' https://api.bintray.com/content/maif/binaries/izanami.jar/${BINARIES_VERSION}/izanami.jar
     curl -T ./izanami-server/target/universal/izanami.zip -u${BINTRAY_USER}:${BINTRAY_PASSWORD} -H 'X-Bintray-Publish: 1' -H 'X-Bintray-Override: 1' -H 'X-Bintray-Version: ${BINARIES_VERSION}' -H 'X-Bintray-Package: izanami-dist' https://api.bintray.com/content/maif/binaries/izanami-dist/${BINARIES_VERSION}/izanami-dist.zip
 fi
 
+sbt -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION ";izanami-server/assembly;izanami-server/dist"
+sbt -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION ";izanami-server/docker:publish"
+echo "Uploading izanami.jar"
+curl -T ./izanami-server/target/scala-2.12/izanami.jar -u${BINTRAY_USER}:${BINTRAY_PASSWORD} -H 'X-Bintray-Publish: 1' -H 'X-Bintray-Override: 1' -H 'X-Bintray-Version: latest' -H 'X-Bintray-Package: izanami.jar' https://api.bintray.com/content/maif/binaries/izanami.jar/latest/izanami.jar
+curl -T ./izanami-server/target/universal/izanami.zip -u${BINTRAY_USER}:${BINTRAY_PASSWORD} -H 'X-Bintray-Publish: 1' -H 'X-Bintray-Override: 1' -H 'X-Bintray-Version: latest' -H 'X-Bintray-Package: izanami-dist' https://api.bintray.com/content/maif/binaries/izanami-dist/latest/izanami-dist.zip
