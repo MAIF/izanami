fc2574198e997b33af7393911820ec4e20071ac2
adelegue <adelegue@hotmail.com>
2017-12-29 11:14:02 +0100

    CI for release

--- scripts/publish-npm-bintray.sh
+++ scripts/publish-npm-bintray.sh
@@ -5,25 +5,19 @@ echo 'Publishing npm packages to bintray'
 LOCATION=`pwd`
 
 cd ${LOCATION}/izanami-clients/react
-echo "registry=https://api.bintray.com/npm/maif/npm" > .npmrc
-echo "_auth=${BINTRAY_NPM_TOKEN}" >> .npmrc
-echo "always-auth=true" >> .npmrc
-echo "email=adelegue@hotmail.com" >> .npmrc
-
+cp ${LOCATION}/.npmrc ${LOCATION}/izanami-clients/react/
 echo 'Unpublishing izanami-react'
 npm unpublish --force --registry https://api.bintray.com/npm/maif/npm
 echo 'Publishing izanami-react'
 npm publish --registry https://api.bintray.com/npm/maif/npm
+rm ${LOCATION}/izanami-clients/react/.npmrc
 
 cd ${LOCATION}/izanami-clients/node
-echo "registry=https://api.bintray.com/npm/maif/npm" > .npmrc
-echo "_auth=${BINTRAY_NPM_TOKEN}" >> .npmrc
-echo "always-auth=true" >> .npmrc
-echo "email=adelegue@hotmail.com" >> .npmrc
-
+cp ${LOCATION}/.npmrc ${LOCATION}/izanami-clients/node/
 echo 'Unpublishing izanami-node'
 npm unpublish --force --registry https://api.bintray.com/npm/maif/npm
 echo 'Publishing izanami-node'
 npm publish --registry https://api.bintray.com/npm/maif/npm
+rm ${LOCATION}/izanami-clients/node/.npmrc
 
 cd ${LOCATION}
