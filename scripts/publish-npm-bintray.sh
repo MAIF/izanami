6cd1c489b65063ac2c6e9c3f16ab600ca7db352d
adelegue <adelegue@hotmail.com>
2017-12-29 09:34:13 +0100

    v0.0.2

---
+++ scripts/publish-npm-bintray.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo 'Publishing npm packages'
+
+LOCATION=`pwd`
+
+echo "//registry.npmjs.org/:_password=${NPM_PASSWORD}" > ~/.npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_AUTH}" >> ~/.npmrc
+echo "//registry.npmjs.org/:username=adelegue" >> ~/.npmrc
+echo "//registry.npmjs.org/:email=aadelegue@gmail.com" >> ~/.npmrc
+
+cd ${LOCATION}/izanami-clients/react
+#npm unpublish --force
+npm publish
+
+cd ${LOCATION}/izanami-clients/node
+#npm unpublish --force
+npm publish
+
+cd ${LOCATION}
