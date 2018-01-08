65b9ab44c6f056443660caae41f97932234cc682
adelegue <adelegue@hotmail.com>
2018-01-08 15:42:21 +0100

    Handling multi format for configs

--- izanami-clients/jvm/src/main/scala/izanami/scaladsl/scaladsl.scala
+++ izanami-clients/jvm/src/main/scala/izanami/scaladsl/scaladsl.scala
@@ -11,16 +11,8 @@ import izanami.IzanamiBackend.SseBackend
 import izanami.Strategy.DevStrategy
 import izanami._
 import izanami.commons.{HttpClient, IzanamiException}
-import izanami.configs.{
-  FallbackConfigStategy,
-  FetchConfigClient,
-  FetchWithCacheConfigClient,
-  SmartCacheConfigClient
-}
-import izanami.experiments.{
-  FallbackExperimentStrategy,
-  FetchExperimentsStrategy
-}
+import izanami.configs.{FallbackConfigStategy, FetchConfigClient, FetchWithCacheConfigClient, SmartCacheConfigClient}
+import izanami.experiments.{FallbackExperimentStrategy, FetchExperimentsStrategy}
 import izanami.features.{
   FallbackFeatureStategy,
   FetchFeatureClient,
@@ -36,22 +28,18 @@ import scala.util.Try
 
 object IzanamiClient {
 
-  def apply(config: ClientConfig)(
-      implicit actorSystem: ActorSystem): IzanamiClient =
+  def apply(config: ClientConfig)(implicit actorSystem: ActorSystem): IzanamiClient =
     new IzanamiClient(config)
 
 }
 
-class IzanamiClient(val config: ClientConfig)(
-    implicit val actorSystem: ActorSystem) {
+class IzanamiClient(val config: ClientConfig)(implicit val actorSystem: ActorSystem) {
   import akka.event.Logging
 
   private val client: HttpClient = HttpClient(actorSystem, config)
 
   private implicit val materializer: Materializer =
-    ActorMaterializer(
-      ActorMaterializerSettings(actorSystem).withDispatcher(config.dispatcher))(
-      actorSystem)
+    ActorMaterializer(ActorMaterializerSettings(actorSystem).withDispatcher(config.dispatcher))(actorSystem)
 
   implicit val izanamiDispatcher =
     IzanamiDispatcher(config.dispatcher, actorSystem)
@@ -61,7 +49,8 @@ class IzanamiClient(val config: ClientConfig)(
       () =>
         client
           .eventStream()
-          .runWith(BroadcastHub.sink(1024)))
+          .runWith(BroadcastHub.sink(1024))
+    )
     .mapMaterializedValue(_ => NotUsed)
 
   def configClient(
@@ -71,9 +60,7 @@ class IzanamiClient(val config: ClientConfig)(
     val source = config.backend match {
       case SseBackend => eventSource
       case _ =>
-        Source.failed(
-          new IllegalStateException(
-            "Notifications are disabled for this strategy"))
+        Source.failed(new IllegalStateException("Notifications are disabled for this strategy"))
     }
     strategy match {
       case DevStrategy =>
@@ -81,37 +68,22 @@ class IzanamiClient(val config: ClientConfig)(
       case Strategy.FetchStrategy =>
         FetchConfigClient(client, config, fallback, source)
       case s: Strategy.FetchWithCacheStrategy =>
-        FetchWithCacheConfigClient(
-          config,
-          fallback,
-          FetchConfigClient(client, config, fallback, source),
-          s)
+        FetchWithCacheConfigClient(config, fallback, FetchConfigClient(client, config, fallback, source), s)
       case s: Strategy.CacheWithSseStrategy =>
-        SmartCacheConfigClient(
-          config,
-          FetchConfigClient(client, config, fallback, eventSource),
-          fallback,
-          s)
+        SmartCacheConfigClient(config, FetchConfigClient(client, config, fallback, eventSource), fallback, s)
       case s: Strategy.CacheWithPollingStrategy =>
-        SmartCacheConfigClient(
-          config,
-          FetchConfigClient(client, config, fallback, source),
-          fallback,
-          s)
+        SmartCacheConfigClient(config, FetchConfigClient(client, config, fallback, source), fallback, s)
     }
   }
 
   def featureClient(
       strategy: Strategy,
-      fallback: ClientConfig => Features = clientConfig =>
-        Features(clientConfig, Seq.empty, Seq.empty)
+      fallback: ClientConfig => Features = clientConfig => Features(clientConfig, Seq.empty, Seq.empty)
   ) = {
     val source = config.backend match {
       case SseBackend => eventSource
       case _ =>
-        Source.failed(
-          new IllegalStateException(
-            "Notifications are disabled for this strategy"))
+        Source.failed(new IllegalStateException("Notifications are disabled for this strategy"))
     }
     val fb = fallback(config)
     strategy match {
@@ -120,41 +92,27 @@ class IzanamiClient(val config: ClientConfig)(
       case Strategy.FetchStrategy =>
         FetchFeatureClient(client, config, fb, source)
       case s: Strategy.FetchWithCacheStrategy =>
-        FetchWithCacheFeatureClient(
-          config,
-          FetchFeatureClient(client, config, fb, source),
-          s)
+        FetchWithCacheFeatureClient(config, FetchFeatureClient(client, config, fb, source), s)
       case s: Strategy.CacheWithSseStrategy =>
-        SmartCacheFeatureClient(
-          config,
-          FetchFeatureClient(client, config, fallback(config), eventSource),
-          fb,
-          s)
+        SmartCacheFeatureClient(config, FetchFeatureClient(client, config, fallback(config), eventSource), fb, s)
       case s: Strategy.CacheWithPollingStrategy =>
-        SmartCacheFeatureClient(config,
-                                FetchFeatureClient(client, config, fb, source),
-                                fb,
-                                s)
+        SmartCacheFeatureClient(config, FetchFeatureClient(client, config, fb, source), fb, s)
     }
   }
 
-  def experimentClient(strategy: Strategy,
-                       fallback: Experiments = Experiments()) =
+  def experimentClient(strategy: Strategy, fallback: Experiments = Experiments()) =
     strategy match {
       case DevStrategy =>
         FallbackExperimentStrategy(fallback)
       case Strategy.FetchStrategy =>
         FetchExperimentsStrategy(client, fallback)
       case s =>
-        throw new IllegalArgumentException(
-          s"This strategy $s is not not supported for experiments")
+        throw new IllegalArgumentException(s"This strategy $s is not not supported for experiments")
     }
 
   def proxy(): Proxy = Proxy(None, None, None)
 
-  def proxy(featureClient: FeatureClient,
-            configClient: ConfigClient,
-            experimentClient: ExperimentsClient): Proxy =
+  def proxy(featureClient: FeatureClient, configClient: ConfigClient, experimentClient: ExperimentsClient): Proxy =
     Proxy(Some(featureClient), Some(configClient), Some(experimentClient))
 
 }
@@ -178,13 +136,11 @@ case class Proxy(
   def withExperimentsClient(experimentsClient: ExperimentsClient) =
     this.copy(experimentClient = Some(experimentsClient))
   def withFeaturePattern(pattern: String) = this.copy(featurePattern = pattern)
-  def withConfigPattern(pattern: String) = this.copy(configPattern = pattern)
+  def withConfigPattern(pattern: String)  = this.copy(configPattern = pattern)
   def withExperimentPattern(pattern: String) =
     this.copy(experimentPattern = pattern)
 
-  def statusAndJsonResponse(
-      context: Option[JsObject] = None,
-      userId: Option[String] = None): Future[(Int, JsValue)] = {
+  def statusAndJsonResponse(context: Option[JsObject] = None, userId: Option[String] = None): Future[(Int, JsValue)] = {
 
     require(context != null, "context should not be null")
     require(userId != null, "userId should not be null")
@@ -213,8 +169,7 @@ case class Proxy(
         _.configs(configPattern)
           .map(c => Json.obj("configurations" -> c.tree()))
       )
-      .getOrElse(
-        FastFuture.successful(Json.obj("configurations" -> Json.obj())))
+      .getOrElse(FastFuture.successful(Json.obj("configurations" -> Json.obj())))
 
     val experiments: Future[JsObject] = userId
       .flatMap(id => experimentClient.map(_.tree(experimentPattern, id)))
@@ -235,15 +190,12 @@ case class Proxy(
 
   }
 
-  def statusAndStringResponse(
-      context: Option[JsObject] = None,
-      userId: Option[String] = None): Future[(Int, String)] =
+  def statusAndStringResponse(context: Option[JsObject] = None, userId: Option[String] = None): Future[(Int, String)] =
     statusAndJsonResponse(context, userId).map {
       case (status, json) => (status, Json.stringify(json))
     }
 
-  def markVariantDisplayed(experimentId: String,
-                           clientId: String): Future[(Int, JsValue)] =
+  def markVariantDisplayed(experimentId: String, clientId: String): Future[(Int, JsValue)] =
     experimentClient
       .map(
         _.markVariantDisplayed(experimentId, clientId)
@@ -253,20 +205,17 @@ case class Proxy(
           .recover {
             case e =>
               logger.error(e, "Error while marking variant displayed")
-              (400,
-               Json.obj("errors" -> "Error while marking variant displayed"))
+              (400, Json.obj("errors" -> "Error while marking variant displayed"))
           }
       )
       .getOrElse(FastFuture.successful((200, Json.obj())))
 
-  def markVariantDisplayedStringResp(experimentId: String,
-                                     clientId: String): Future[(Int, String)] =
+  def markVariantDisplayedStringResp(experimentId: String, clientId: String): Future[(Int, String)] =
     markVariantDisplayed(experimentId, clientId).map {
       case (status, json) => (status, Json.stringify(json))
     }
 
-  def markVariantWon(experimentId: String,
-                     clientId: String): Future[(Int, JsValue)] =
+  def markVariantWon(experimentId: String, clientId: String): Future[(Int, JsValue)] =
     experimentClient
       .map(
         _.markVariantWon(experimentId, clientId)
@@ -276,14 +225,12 @@ case class Proxy(
           .recover {
             case e =>
               logger.error(e, "Error while marking variant displayed")
-              (400,
-               Json.obj("errors" -> "Error while marking variant displayed"))
+              (400, Json.obj("errors" -> "Error while marking variant displayed"))
           }
       )
       .getOrElse(FastFuture.successful((200, Json.obj())))
 
-  def markVariantWonStringResp(experimentId: String,
-                               clientId: String): Future[(Int, String)] =
+  def markVariantWonStringResp(experimentId: String, clientId: String): Future[(Int, String)] =
     markVariantWon(experimentId, clientId).map {
       case (status, json) => (status, Json.stringify(json))
     }
@@ -311,8 +258,7 @@ trait FeatureClient {
         case false => ko
       }(izanamiDispatcher.ec)
 
-  def featureOrElse[T](key: String, context: JsObject)(ok: => T)(
-      ko: => T): Future[T] =
+  def featureOrElse[T](key: String, context: JsObject)(ok: => T)(ko: => T): Future[T] =
     checkFeature(key, context)
       .map {
         case true  => ok
@@ -346,7 +292,7 @@ trait Registration {
 
 case class FakeRegistration() extends Registration {
   override def onComplete(cb: Try[Done] => Unit): Unit = {}
-  override def close(): Unit = {}
+  override def close(): Unit                           = {}
 }
 
 case class DefaultRegistration(killSwitch: UniqueKillSwitch, done: Future[Done])(
@@ -381,9 +327,7 @@ object Features {
       .parse(json)
       .validate[Seq[DefaultFeature]]
       .fold(
-        err =>
-          throw IzanamiException(
-            s"Error parsing json $json: \n${Json.prettyPrint(JsError.toJson(err))}"),
+        err => throw IzanamiException(s"Error parsing json $json: \n${Json.prettyPrint(JsError.toJson(err))}"),
         identity
       )
     config =>
@@ -391,9 +335,7 @@ object Features {
   }
 }
 
-case class Features(clientConfig: ClientConfig,
-                    featuresSeq: Seq[Feature],
-                    fallback: Seq[Feature] = Seq.empty) {
+case class Features(clientConfig: ClientConfig, featuresSeq: Seq[Feature], fallback: Seq[Feature] = Seq.empty) {
   def isActive(key: String): Boolean =
     featuresSeq
       .find(_.id == key)
@@ -411,9 +353,8 @@ case class Features(clientConfig: ClientConfig,
   }
 
   private def json(feature: Feature): JsObject = {
-    val jsPath: JsPath = feature.id.split(":").foldLeft[JsPath](JsPath) {
-      (p, s) =>
-        p \ s
+    val jsPath: JsPath = feature.id.split(":").foldLeft[JsPath](JsPath) { (p, s) =>
+      p \ s
     }
     (jsPath \ "active").write[Boolean].writes(feature.isActive(clientConfig))
   }
@@ -428,22 +369,20 @@ object Config {
   import play.api.libs.functional.syntax._
   private val read = (
     (__ \ "id").read[String] and
-      (__ \ "value").read[String].map(s => Json.parse(s))
+    (__ \ "value").read[String].map(s => Json.parse(s)).orElse {
+      (__ \ "value").read[JsValue]
+    }
   )(Config.apply _)
 
   private val write: Writes[Config] = Writes[Config] { c =>
     Json.obj(
-      "id" -> c.id,
+      "id"    -> c.id,
       "value" -> Json.stringify(c.value)
     )
   }
 
   implicit val format = Format(read, write)
 
-  val alternateRead = (
-    (__ \ "id").read[String] and
-      (__ \ "value").read[JsValue]
-  )(Config.apply _)
 }
 case class Config(id: String, value: JsValue) {
   def asJson =
@@ -463,20 +402,16 @@ object Configs {
   def apply(configs: (String, JsValue)*): Configs =
     new Configs(configs.map(c => Config(c._1, c._2)), fallback = Seq.empty)
 
-  def parseJson(json: String): Configs = {
-    implicit val r = Config.alternateRead
+  def parseJson(json: String): Configs =
     Configs(
       Json
         .parse(json)
         .validate[Seq[Config]]
         .fold(
-          err =>
-            throw IzanamiException(
-              s"Error parsing json $json: \n${Json.prettyPrint(JsError.toJson(err))}"),
+          err => throw IzanamiException(s"Error parsing json $json: \n${Json.prettyPrint(JsError.toJson(err))}"),
           identity
         )
     )
-  }
 
 }
 
@@ -505,10 +440,9 @@ sealed trait ConfigEvent extends Event {
 }
 
 object ConfigEvent {
-  case class ConfigCreated(id: String, config: Config) extends ConfigEvent
-  case class ConfigUpdated(id: String, config: Config, oldConfig: Config)
-      extends ConfigEvent
-  case class ConfigDeleted(id: String) extends ConfigEvent
+  case class ConfigCreated(id: String, config: Config)                    extends ConfigEvent
+  case class ConfigUpdated(id: String, config: Config, oldConfig: Config) extends ConfigEvent
+  case class ConfigDeleted(id: String)                                    extends ConfigEvent
 }
 
 trait ConfigClient {
@@ -552,30 +486,25 @@ trait ExperimentsClient {
 
   def tree(pattern: String, clientId: String): Future[JsObject]
 
-  def getVariantFor(experimentId: String,
-                    clientId: String): Future[Option[Variant]]
+  def getVariantFor(experimentId: String, clientId: String): Future[Option[Variant]]
 
-  def markVariantDisplayed(experimentId: String,
-                           clientId: String): Future[ExperimentVariantDisplayed]
+  def markVariantDisplayed(experimentId: String, clientId: String): Future[ExperimentVariantDisplayed]
 
-  def markVariantWon(experimentId: String,
-                     clientId: String): Future[ExperimentVariantWon]
+  def markVariantWon(experimentId: String, clientId: String): Future[ExperimentVariantWon]
 }
 
-case class ExperimentClient(experimentsClient: ExperimentsClient,
-                            experiment: Experiment) {
+case class ExperimentClient(experimentsClient: ExperimentsClient, experiment: Experiment) {
 
-  def id: String = experiment.id
-  def name: String = experiment.name
-  def description: String = experiment.description
-  def enabled: Boolean = experiment.enabled
+  def id: String             = experiment.id
+  def name: String           = experiment.name
+  def description: String    = experiment.description
+  def enabled: Boolean       = experiment.enabled
   def variants: Seq[Variant] = experiment.variants
 
   def getVariantFor(clientId: String): Future[Option[Variant]] =
     experimentsClient.getVariantFor(experiment.id, clientId)
 
-  def markVariantDisplayed(
-      clientId: String): Future[ExperimentVariantDisplayed] =
+  def markVariantDisplayed(clientId: String): Future[ExperimentVariantDisplayed] =
     experimentsClient.markVariantDisplayed(experiment.id, clientId)
 
   def markVariantWon(clientId: String): Future[ExperimentVariantWon] =
