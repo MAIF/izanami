dfaf7b30b86649668e0df34cbda296d7ed0d4b5d
adelegue <adelegue@hotmail.com>
2018-01-10 14:41:21 +0100

    CI for rust

--- scripts/build-javascript.sh
+++ scripts/build-javascript.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-if test "$TRAVIS_OS_NAME" = "linux"
-then
-    LOCATION=`pwd`
+LOCATION=`pwd`
 
-    cd $LOCATION/izanami-server/javascript
-    yarn install
-    yarn build
-fi
+cd $LOCATION/izanami-server/javascript
+yarn install
+yarn build
