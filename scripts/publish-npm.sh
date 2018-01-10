1e7bc18e64af454e3051a0acc7464b0fa8f06363
adelegue <adelegue@hotmail.com>
2018-01-10 14:41:21 +0100

    CI for rust

--- scripts/publish-npm.sh
+++ scripts/publish-npm.sh
@@ -2,57 +2,54 @@
 
 LOCATION=`pwd`
 
-if test "$TRAVIS_OS_NAME" = "linux"
+if test "$TRAVIS_PULL_REQUEST" = "false"
 then
-    if test "$TRAVIS_PULL_REQUEST" = "false"
+    if [ -z "$TRAVIS_TAG" ];
     then
-        if [ -z "$TRAVIS_TAG" ];
-        then
-            echo 'Not a tag publishing beta to npm registry'
-            yarn config set version-git-tag false
-            cd ${LOCATION}/izanami-clients/react
-            npm install
-            PACKAGE_CURRENT_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g')
-            PACKAGE_VERSION="${PACKAGE_CURRENT_VERSION}-alpha.${TRAVIS_BUILD_NUMBER}"
-            npm version ${PACKAGE_VERSION}
+        echo 'Not a tag publishing beta to npm registry'
+        yarn config set version-git-tag false
+        cd ${LOCATION}/izanami-clients/react
+        npm install
+        PACKAGE_CURRENT_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g')
+        PACKAGE_VERSION="${PACKAGE_CURRENT_VERSION}-alpha.${TRAVIS_BUILD_NUMBER}"
+        npm version ${PACKAGE_VERSION}
 
-            echo "//registry.npmjs.org/:_password=${NPM_PASSWORD}" > .npmrc
-            echo "//registry.npmjs.org/:_authToken=${NPM_AUTH}" >> .npmrc
-            echo "//registry.npmjs.org/:username=adelegue" >> .npmrc
-            echo "//registry.npmjs.org/:email=aadelegue@gmail.com" >> .npmrc
+        echo "//registry.npmjs.org/:_password=${NPM_PASSWORD}" > .npmrc
+        echo "//registry.npmjs.org/:_authToken=${NPM_AUTH}" >> .npmrc
+        echo "//registry.npmjs.org/:username=adelegue" >> .npmrc
+        echo "//registry.npmjs.org/:email=aadelegue@gmail.com" >> .npmrc
 
-            npm publish
+        npm publish
 
-            cd ${LOCATION}/izanami-clients/node
+        cd ${LOCATION}/izanami-clients/node
 
-            PACKAGE_CURRENT_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g')
-            PACKAGE_VERSION="${PACKAGE_CURRENT_VERSION}-alpha.${TRAVIS_BUILD_NUMBER}"
-            npm version ${PACKAGE_VERSION}
+        PACKAGE_CURRENT_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g')
+        PACKAGE_VERSION="${PACKAGE_CURRENT_VERSION}-alpha.${TRAVIS_BUILD_NUMBER}"
+        npm version ${PACKAGE_VERSION}
 
-            echo "//registry.npmjs.org/:_password=${NPM_PASSWORD}" > .npmrc
-            echo "//registry.npmjs.org/:_authToken=${NPM_AUTH}" >> .npmrc
-            echo "//registry.npmjs.org/:username=adelegue" >> .npmrc
-            echo "//registry.npmjs.org/:email=aadelegue@gmail.com" >> .npmrc
+        echo "//registry.npmjs.org/:_password=${NPM_PASSWORD}" > .npmrc
+        echo "//registry.npmjs.org/:_authToken=${NPM_AUTH}" >> .npmrc
+        echo "//registry.npmjs.org/:username=adelegue" >> .npmrc
+        echo "//registry.npmjs.org/:email=aadelegue@gmail.com" >> .npmrc
 
-            npm publish
-        else
-            echo "Publishing npm packages for tag ${TRAVIS_TAG}"
-            yarn config set version-git-tag false
+        npm publish
+    else
+        echo "Publishing npm packages for tag ${TRAVIS_TAG}"
+        yarn config set version-git-tag false
 
-            echo "//registry.npmjs.org/:_password=${NPM_PASSWORD}" > ~/.npmrc
-            echo "//registry.npmjs.org/:_authToken=${NPM_AUTH}" >> ~/.npmrc
-            echo "//registry.npmjs.org/:username=adelegue" >> ~/.npmrc
-            echo "//registry.npmjs.org/:email=aadelegue@gmail.com" >> ~/.npmrc
+        echo "//registry.npmjs.org/:_password=${NPM_PASSWORD}" > ~/.npmrc
+        echo "//registry.npmjs.org/:_authToken=${NPM_AUTH}" >> ~/.npmrc
+        echo "//registry.npmjs.org/:username=adelegue" >> ~/.npmrc
+        echo "//registry.npmjs.org/:email=aadelegue@gmail.com" >> ~/.npmrc
 
-            cd ${LOCATION}/izanami-clients/react
-            echo "${TRAVIS_TAG}" | cut -d "v" -f 2 | yarn version
-            npm install
-            npm publish
+        cd ${LOCATION}/izanami-clients/react
+        echo "${TRAVIS_TAG}" | cut -d "v" -f 2 | yarn version
+        npm install
+        npm publish
 
-            cd ${LOCATION}/izanami-clients/node
-            echo "${TRAVIS_TAG}" | cut -d "v" -f 2 | yarn version
-            npm publish
-        fi
+        cd ${LOCATION}/izanami-clients/node
+        echo "${TRAVIS_TAG}" | cut -d "v" -f 2 | yarn version
+        npm publish
     fi
 fi
 
