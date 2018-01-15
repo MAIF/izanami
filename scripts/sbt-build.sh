7b5c9395c9eccf1fd740374de1ec7c2cd0c57b7b
adelegue <adelegue@hotmail.com>
2018-01-15 15:21:31 +0100

    release process

--- scripts/sbt-build.sh
+++ scripts/sbt-build.sh
@@ -14,7 +14,7 @@ then
 else
 echo "Tag ${TRAVIS_TAG}, Publishing client"
     sbt -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION ";izanami-server/assembly;izanami-server/dist;izanami-server/docker:publish"
-    sbt -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION "+publish"
+    sbt -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION "+jvm/publish"
     echo "Uploading izanami.jar"
     curl -T ./izanami-server/target/scala-2.12/izanami.jar -u${BINTRAY_USER}:${BINTRAY_PASSWORD} -H "X-Bintray-Publish: 1" -H "X-Bintray-Override: 1" -H "X-Bintray-Version: latest" -H "X-Bintray-Package: izanami.jar" https://api.bintray.com/content/maif/binaries/izanami.jar/latest/izanami.jar
     curl -T ./izanami-server/target/universal/izanami.zip -u${BINTRAY_USER}:${BINTRAY_PASSWORD} -H "X-Bintray-Publish: 1" -H "X-Bintray-Override: 1" -H "X-Bintray-Version: latest" -H "X-Bintray-Package: izanami-dist" https://api.bintray.com/content/maif/binaries/izanami-dist/latest/izanami-dist.zip
