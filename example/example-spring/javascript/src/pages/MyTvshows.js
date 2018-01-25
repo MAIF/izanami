6a6b0c45b09d1445719b1f6f051edaed62437a5a
Baudelot Philippe <philippe@baudelot.eu>
2018-01-25 10:56:06 +0100

    add A/B testing skin and ADD skin

--- example/example-spring/javascript/src/pages/MyTvshows.js
+++ example/example-spring/javascript/src/pages/MyTvshows.js
@@ -22,14 +22,18 @@ export default class MyTvShows extends React.Component {
                 {this.props.user.shows.map(({image, title, description, id}) =>
                   <div className="media" key={`shows-${id}`}>
                     <div className="media-left media-middle">
-                        <img className="media-object" width="440px" src={`${image}`} />
+                        <img className="media-object" width="540px" src={`${image}`} />
                     </div>
                     <div className="media-body">
-                      <h4 className="media-heading">{title}</h4>
-
+                      <h3 className="media-heading">{title}</h3>
+                      {/* A/B testing eye top*/}
                       <Link to={`/tvshow/${id}`} className="btn pull-right" alt="consulter"><i className="fa fa-eye"></i></Link>
+
                       <button type="button" className="btn pull-right" onClick={this.remove(id)} alt="supprimer"><i className="glyphicon glyphicon-trash"/></button>
                       <p className="description">{description}</p>
+                      {/* A/B testing eye bottom*/}
+                      <Link to={`/tvshow/${id}`} className="btn pull-right" alt="consulter"><i className="fa fa-eye"></i></Link>
+
                     </div>
                   </div>
                 )}
