e1baa21c1bf74effb8e80999888353e2c80a5dbb
adelegue <adelegue@hotmail.com>
2018-01-29 16:59:31 +0100

    Configurable API KEYS

--- izanami-server/test/domains/script/ScriptSpec.scala
+++ izanami-server/test/domains/script/ScriptSpec.scala
@@ -63,7 +63,7 @@ class ScriptSpec extends PlaySpec with OneServerPerSuiteWithComponents with Scal
 
   val config = IzanamiConfig(
     Some("dev"),
-    Default(DefaultFilter(Seq(), "", "", "", ApiKey("", ""))),
+    Default(DefaultFilter(Seq(), "", "", "", ApiKeyHeaders("", ""))),
     DbConfig("", None, None, None, None, None),
     LogoutConfig(""),
     ConfigConfig(DbDomainConfig("", DbDomainConfigDetails(""))),
