e622ab4de26dcecb334a50c0af6db976f4b7067f
adelegue <adelegue@hotmail.com>
2018-02-22 10:14:59 +0100

    Build script

--- scripts/build-rust.sh
+++ scripts/build-rust.sh
@@ -14,6 +14,8 @@ else
     CLI_VERSION="${BINARIES_VERSION}"
 fi
 
+echo "Releasing rust with version: ${CLI_VERSION}"
+
 curl -T ./target/release/izanami-cli -u${BINTRAY_USER}:${BINTRAY_PASSWORD} -H "X-Bintray-Publish: 1" -H "X-Bintray-Override: 1" -H "X-Bintray-Version: ${CLI_VERSION}" -H "X-Bintray-Package: ${TRAVIS_OS_NAME}-izanamicli" https://api.bintray.com/content/maif/binaries/${TRAVIS_OS_NAME}-izanamicli/${CLI_VERSION}/izanami-cli
 
 cd $LOCATION
