037bc97cead61507293cfbb7629f7d158eadd687
adelegue <adelegue@hotmail.com>
2018-02-23 17:33:31 +0100

    Key picker

--- izanami-server/javascript/src/izanami/inputs/index.js
+++ izanami-server/javascript/src/izanami/inputs/index.js
@@ -4,6 +4,7 @@ export * from './LinkDisplay';
 export * from './SelectInput';
 export * from './BooleanInput';
 export * from './TextInput';
+export * from './KeyInput';
 export * from './NumberInput';
 export * from './Form';
 export * from './Table';
