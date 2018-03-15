9568b37e25346cbf28fd4360d02a1a9d46ac83e3
adelegue <adelegue@hotmail.com>
2018-03-15 17:26:18 +0100

    Clean example

--- example/example-spring/javascript-angular/src/app/izanami/feature.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FeatureDirective } from './feature.directive';
-
-describe('FeatureDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FeatureDirective();
-    expect(directive).toBeTruthy();
-  });
-});
