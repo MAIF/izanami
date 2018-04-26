61d8eef8777d093fe78061627d3fcd8bb961c632
Baudelot Philippe <philippe@baudelot.eu>
2018-04-26 10:00:05 +0200

    skin percentage

--- izanami-server/javascript/src/izanami/inputs/PercentageInput.js
+++ izanami-server/javascript/src/izanami/inputs/PercentageInput.js
@@ -10,13 +10,10 @@ export class PercentageInput extends Component {
   render() {
     return (
       <div className="form-group">
-        <label htmlFor="exampleInputAmount" className="col-sm-2 control-label">{this.props.label}</label>
-        <div className="col-sm-10">
-            <div className="input-group">
+        <label htmlFor="exampleInputAmount" className="col-sm-2 control-label">{this.props.label} (%)</label>
+        <div className="col-sm-2">
               <input type={'number'} className="form-control" id={`input-${this.props.label}`}
                      placeholder={this.props.placeholder} value={this.props.value} onChange={this.onChange} />
-                <div className="input-group-addon">%</div>
-            </div>
         </div>
       </div>
     );
