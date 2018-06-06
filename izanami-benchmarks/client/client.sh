5870d76e10e87e0207ec1297aec4d33078bbd5e6
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-06-06 16:57:35 +0200

    Sleep longer

--- izanami-benchmarks/client/client.sh
+++ izanami-benchmarks/client/client.sh
@@ -2,7 +2,7 @@ export GOPATH=/gopath
 export PATH=$PATH:/usr/local/go/bin:/gopath/bin
 
 echo "Sleeping"
-sleep 10
+sleep 20
 echo "Warm up ..."
 wrk -R 500 -t6 -c200 -d40s -H "Izanami-Client-Id: apikey" -H "Izanami-Client-Secret: 123456" http://izanami:8080/api/features
 echo "Warm up done !"
