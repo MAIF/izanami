63f1358dade8e7cece89529e16b97a9e787e7cff
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-06-06 16:45:57 +0200

    Add hey test

--- izanami-benchmarks/client/client.sh
+++ izanami-benchmarks/client/client.sh
@@ -1,3 +1,6 @@
+export GOPATH=/gopath
+export PATH=$PATH:/usr/local/go/bin:/gopath/bin
+
 echo "Sleeping"
 sleep 10
 echo "Warm up ..."
@@ -5,4 +8,7 @@ wrk -R 500 -t6 -c200 -d40s -H "Izanami-Client-Id: apikey" -H "Izanami-Client-Sec
 echo "Warm up done !"
 sleep 10
 echo "Bench ..."
-wrk -R 2000 -200 -c800 -d40s -H "Izanami-Client-Id: apikey" -H "Izanami-Client-Secret: 123456" --latency http://izanami:8080/api/features
+wrk -R 2000 -t200 -c800 -d40s -H "Izanami-Client-Id: apikey" -H "Izanami-Client-Secret: 123456" --latency http://izanami:8080/api/features
+
+echo "Hey ..."
+hey -H "Izanami-Client-Id: apikey" -H "Izanami-Client-Secret: 123456" -n 1000 -c 400 -m GET http://localhost:8080/api/features
