05eeb30900ab717d0879d17ff887bc2927495e04
Gauthier POULET <gauthier.poulet@gmail.com>
2018-06-17 18:43:52 +0200

    [Clean code] add propTypes to <Debug /> component

--- izanami-clients/react/src/features.js
+++ izanami-clients/react/src/features.js
@@ -2,7 +2,7 @@ import 'es6-shim';
 import 'whatwg-fetch';
 import Symbol from 'es-symbol';
 import React, { Component } from 'react';
-import { func, string, bool, object } from 'prop-types';
+import { func, string, bool, object, node } from 'prop-types';
 import deepEqual from 'deep-equal';
 import deepmerge from 'deepmerge';
 import { get, isFunction } from 'lodash';
@@ -25,16 +25,25 @@ export class Disabled extends Component {
   }
 }
 
-function Debug ({ isActive, path, children }) {
-  return (
-    <div className={`izanami-feature-${isActive ? 'enabled' : 'disabled'}`} title={`Feature ${path} is ${isActive ? 'enabled' : 'disabled'}`} style={{ position: 'relative', outline: '1px solid green' }}>
-      <span style={{ padding: 2, fontFamily: 'Arial', color: 'white', border: '1px solid black',  borderRadius: '5px', backgroundColor: isActive ? 'green' : 'grey', position: 'absolute', top: -17, left: -1, zIndex: 100000, boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.3), 0 6px 20px 0 rgba(0, 0, 0, 0.19)' }}>
-        Feature <span style={{ fontWeight: 'bold' }}>{path}</span> is {isActive ? 'enabled' : 'disabled'}
-      </span>
-      { children }
-    </div>
-  );
+class Debug extends Component {
 
+  static propTypes = {
+    path: string,
+    isActive: bool,
+    children: node
+  };
+
+  render() {
+    const { isActive, path, children } = this.props;
+    return (
+      <div className={`izanami-feature-${isActive ? 'enabled' : 'disabled'}`} title={`Feature ${path} is ${isActive ? 'enabled' : 'disabled'}`} style={{ position: 'relative', outline: '1px solid green' }}>
+        <span style={{ padding: 2, fontFamily: 'Arial', color: 'white', border: '1px solid black',  borderRadius: '5px', backgroundColor: isActive ? 'green' : 'grey', position: 'absolute', top: -17, left: -1, zIndex: 100000, boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.3), 0 6px 20px 0 rgba(0, 0, 0, 0.19)' }}>
+          Feature <span style={{ fontWeight: 'bold' }}>{path}</span> is {isActive ? 'enabled' : 'disabled'}
+        </span>
+        { children }
+      </div>
+    );
+  }
 }
 
 export class Feature extends Component {
