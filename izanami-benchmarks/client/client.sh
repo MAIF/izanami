a5e39ef630c37e8e667699e0010b629abd414c01
larousso <adelegue>
2018-07-10 11:31:18 +0200

    2000 connexions during benchs

--- izanami-benchmarks/client/client.sh
+++ izanami-benchmarks/client/client.sh
@@ -8,7 +8,7 @@ wrk -R 500 -t6 -c200 -d40s -H "Izanami-Client-Id: apikey" -H "Izanami-Client-Sec
 echo "Warm up done !"
 sleep 10
 echo "Bench ..."
-wrk -R 2000 -t200 -c800 -d40s -H "Izanami-Client-Id: apikey" -H "Izanami-Client-Secret: 123456" --latency http://izanami:8080/api/features
+wrk -R 2000 -t200 -c2000 -d40s -H "Izanami-Client-Id: apikey" -H "Izanami-Client-Secret: 123456" --latency http://izanami:8080/api/features
 
 #echo "Hey ..."
 #hey -H "Izanami-Client-Id: apikey" -H "Izanami-Client-Secret: 123456" -n 1000 -c 400 -m GET http://izanami:8080/api/features
