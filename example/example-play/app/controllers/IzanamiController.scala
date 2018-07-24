9ed42eabb3a4177248f3fa57c53ded550ea8bbc5
larousso <adelegue>
2018-07-24 09:06:29 +0200

    Playframework tuto

--- example/example-play/app/controllers/IzanamiController.scala
+++ example/example-play/app/controllers/IzanamiController.scala
@@ -1,19 +1,40 @@
 package controllers
 
 import akka.actor.ActorSystem
+import controllers.actions.SecuredAction
 import env.Env
 import izanami.scaladsl.Proxy
+import play.api.libs.json.Json
 import play.api.mvc.{AbstractController, ControllerComponents}
 
-class IzanamiController(env: Env, proxy: Proxy, val cc: ControllerComponents)(implicit system: ActorSystem)
-    extends AbstractController(cc) {
+class IzanamiController(env: Env, proxy: Proxy, SecuredAction: SecuredAction, val cc: ControllerComponents)(
+    implicit system: ActorSystem
+) extends AbstractController(cc) {
 
+  import cats._
+  import cats.implicits._
   import system.dispatcher
 
-  def izanamiProxy() = Action.async { _ =>
-    proxy.statusAndJsonResponse(None, None).map {
-      case (_, response) =>
-        Ok(response)
+  def izanamiProxy() = SecuredAction.async { req =>
+    proxy
+      .statusAndJsonResponse(
+        Json.obj("userId" -> req.userId).some,
+        req.userId.some
+      )
+      .map {
+        case (status, response) => Status(status)(response)
+      }
+  }
+
+  def markDisplayed(experiment: String) = SecuredAction.async { req =>
+    proxy.markVariantDisplayed(experiment, req.userId).map {
+      case (status, response) => Status(status)(response)
+    }
+  }
+
+  def markWon(experiment: String) = SecuredAction.async { req =>
+    proxy.markVariantWon(experiment, req.userId).map {
+      case (status, response) => Status(status)(response)
     }
   }
 
