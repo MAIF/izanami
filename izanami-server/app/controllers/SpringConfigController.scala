9c9d55c865d0747c0faa3989e7a38cfaf47be8ef
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2018-10-11 14:43:41 +0200

    Better naming in response payload

--- izanami-server/app/controllers/SpringConfigController.scala
+++ izanami-server/app/controllers/SpringConfigController.scala
@@ -10,11 +10,14 @@ import com.google.common.base.Charsets
 import controllers.actions.SecuredAuthContext
 import domains.Key
 import domains.config.ConfigService
+import env.IzanamiConfig
 import libs.functional.EitherTSyntax
+import org.apache.commons.codec.binary.Hex
 import play.api.libs.json._
 import play.api.mvc._
 
 class SpringConfigController[F[_]: Effect](configStore: ConfigService[F],
+                                           izanamiConfig: IzanamiConfig,
                                            system: ActorSystem,
                                            AuthAction: ActionBuilder[SecuredAuthContext, AnyContent],
                                            val cc: ControllerComponents)
@@ -26,13 +29,19 @@ class SpringConfigController[F[_]: Effect](configStore: ConfigService[F],
 
   implicit val materializer = ActorMaterializer()(system)
 
-  val digester = MessageDigest.getInstance("MD5")
-  val encoder  = Base64.getEncoder
+  val digester = MessageDigest.getInstance("SHA-256")
+
+  def byteToHexString(bytes: Array[Byte]): String = String.valueOf(Hex.encodeHex(bytes))
 
   def raw(rootKey: String, appName: String, profileName: String): Action[Unit] = AuthAction.asyncF(parse.empty) { ctx =>
     val appConfigKey     = Key(s"$rootKey:$appName:$profileName:spring-config")
     val profileConfigKey = Key(s"$rootKey:spring-profiles:$profileName:spring-config")
     val globalConfigKey  = Key(s"$rootKey:spring-globals:spring-config")
+
+    val host: String = ctx.request.headers
+      .get(izanamiConfig.headerHost)
+      .orElse(ctx.request.headers.get("Host"))
+      .getOrElse("localhost:9000")
 
     val result = for {
       app     <- configStore.getById(appConfigKey)
@@ -49,39 +58,45 @@ class SpringConfigController[F[_]: Effect](configStore: ConfigService[F],
                 .collect { case o: JsObject => o }
                 .map(
                   c =>
-                    Json.obj("name" -> s"izanami://configs/$rootKey/$profileName/$appName/spring-config", "source" -> c)
+                    Json.obj(
+                      "name"   -> s"${ctx.request.protocol}://$host/api/configs/$rootKey:$profileName:$appName:spring-config",
+                      "source" -> c
+                  )
                 ),
               profile
                 .map(_.value)
                 .collect { case o: JsObject => o }
                 .map(
                   c =>
-                    Json.obj("name"   -> s"izanami://configs/$rootKey/spring-profiles/$profileName/spring-config",
-                             "source" -> c)
+                    Json.obj(
+                      "name"   -> s"${ctx.request.protocol}://$host/api/configs/$rootKey:spring-profiles:$profileName:spring-config",
+                      "source" -> c
+                  )
                 ),
               global
                 .map(_.value)
                 .collect { case o: JsObject => o }
                 .map(
-                  c => Json.obj("name" -> s"izanami://configs/$rootKey/spring-globals//spring-config", "source" -> c)
+                  c =>
+                    Json.obj(
+                      "name"   -> s"${ctx.request.protocol}://$host/api/configs/$rootKey:spring-globals:spring-config",
+                      "source" -> c
+                  )
                 )
             ).flatten
           )
           val payload = Json.obj(
             "name"            -> s"$appName",
-            "profiles"        -> Json.arr(s"$profileName"),
+            "profiles"        -> Json.arr(s"://$profileName"),
             "label"           -> JsNull,
             "state"           -> JsNull,
             "propertySources" -> propertySources
           )
-          val version =
-            new String(encoder.encode(digester.digest(Json.stringify(payload).getBytes(Charsets.UTF_8))),
-                       Charsets.UTF_8)
+          val version: String = byteToHexString(digester.digest(Json.stringify(payload).getBytes(Charsets.UTF_8)))
           Ok(payload ++ Json.obj("version" -> version))
         }
       }
     }
-
     result
   }
 }
