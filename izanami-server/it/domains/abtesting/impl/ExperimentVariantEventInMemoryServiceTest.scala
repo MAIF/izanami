a6aa4e58208775f8b8eb452f5c6778943da2c484
larousso <adelegue@hotmail.com>
2019-07-09 14:37:14 +0200

    In memory events

--- izanami-server/it/domains/abtesting/impl/ExperimentVariantEventInMemoryServiceTest.scala
+++ izanami-server/it/domains/abtesting/impl/ExperimentVariantEventInMemoryServiceTest.scala
@@ -1,10 +1,12 @@
 package domains.abtesting.impl
 
-import domains.abtesting.AbstractExperimentServiceTest
-import org.scalatest.FunSuite
+import cats.effect.IO
+import domains.abtesting.{AbstractExperimentServiceTest, ExperimentVariantEventService}
+import domains.events.impl.BasicEventStore
+import env.{DbDomainConfig, DbDomainConfigDetails}
 
 class ExperimentVariantEventInMemoryServiceTest extends AbstractExperimentServiceTest("InMemory") {
-
-
-
+  override def dataStore(dataStore: String): ExperimentVariantEventService[IO] = ExperimentVariantEventInMemoryService[IO](
+    DbDomainConfig(env.InMemory, DbDomainConfigDetails(s"test-events:$dataStore", None), None), new BasicEventStore[IO]
+  )
 }
