3529c026bca1509efc0a0172a157fb4c7843ea90
larousso <adelegue@hotmail.com>
2019-10-25 11:06:34 +0200

    Colors in sbt

--- scripts/sbt-build.sh
+++ scripts/sbt-build.sh
@@ -1,29 +1,27 @@
 #!/usr/bin/env bash
 
-SBT_ARGS=-Dsbt.color=always -Dsbt.supershell=false
-
 if [ -z "$TRAVIS_TAG" ];
 then
     if test "$TRAVIS_PULL_REQUEST" = "false" && test "$TRAVIS_BRANCH" = "master"
     then
         docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
         echo 'Master branch, publishing snapshots'
-        sbt -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION $SBT_ARGS ";test"
-        sbt -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION $SBT_ARGS ";it:test"
-        sbt -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION $SBT_ARGS ";izanami-server/assembly;izanami-server/dist;izanami-server/docker:publish;jvm/publishLocal;jvm/publish"
-        sbt -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION $SBT_ARGS ";jvm/publishLocal;jvm/publish"
+        sbt -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION -Dsbt.color=always -Dsbt.supershell=false ";test"
+        sbt -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION -Dsbt.color=always -Dsbt.supershell=false ";it:test"
+        sbt -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION -Dsbt.color=always -Dsbt.supershell=false ";izanami-server/assembly;izanami-server/dist;izanami-server/docker:publish;jvm/publishLocal;jvm/publish"
+        sbt -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION -Dsbt.color=always -Dsbt.supershell=false ";jvm/publishLocal;jvm/publish"
         echo "Uploading izanami.jar"
         curl -T ./izanami-server/target/scala-2.12/izanami.jar -u${BINTRAY_USER}:${BINTRAY_PASS} -H 'X-Bintray-Publish: 1' -H 'X-Bintray-Override: 1' -H 'X-Bintray-Version: latest' -H 'X-Bintray-Package: izanami.jar' https://api.bintray.com/content/maif/binaries/izanami.jar/latest/izanami.jar
         curl -T ./izanami-server/target/universal/izanami.zip -u${BINTRAY_USER}:${BINTRAY_PASS} -H 'X-Bintray-Publish: 1' -H 'X-Bintray-Override: 1' -H 'X-Bintray-Version: latest' -H 'X-Bintray-Package: izanami-dist' https://api.bintray.com/content/maif/binaries/izanami-dist/latest/izanami-dist.zip
     else
         echo 'Other branch, just test ...'
-        sbt  -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION $SBT_ARGS "test"
-        sbt  -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION $SBT_ARGS "it:test"
+        sbt  -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION -Dsbt.color=always -Dsbt.supershell=false "test"
+        sbt  -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION -Dsbt.color=always -Dsbt.supershell=false "it:test"
     fi
 else
     echo "Tag ${TRAVIS_TAG}, Publishing client"
     docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
-    sbt -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION $SBT_ARGS ";izanami-server/assembly;izanami-server/dist;izanami-server/docker:publish;publish"
+    sbt -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION -Dsbt.color=always -Dsbt.supershell=false ";izanami-server/assembly;izanami-server/dist;izanami-server/docker:publish;publish"
 
     echo "Uploading izanami.jar"
     curl -T ./izanami-server/target/scala-2.12/izanami.jar -u${BINTRAY_USER}:${BINTRAY_PASS} -H "X-Bintray-Publish: 1" -H "X-Bintray-Override: 1" -H "X-Bintray-Version: latest" -H "X-Bintray-Package: izanami.jar" https://api.bintray.com/content/maif/binaries/izanami.jar/latest/izanami.jar
