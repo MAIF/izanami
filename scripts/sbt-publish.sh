4a90798c68bc775cdded8691046c59640d88df5f
larousso <adelegue@hotmail.com>
2019-12-11 21:13:07 +0100

    :green_heart: spring publish

--- scripts/sbt-publish.sh
+++ scripts/sbt-publish.sh
@@ -5,8 +5,8 @@ then
     then
         docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
         echo 'Master branch, publishing snapshots'
-        sbt -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION  -Dsbt.color=always -Dsbt.supershell=false ";izanami-server/assembly;izanami-server/dist;izanami-server/docker:publish;jvm/publishLocal;jvm/publish"
-        sbt -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION  -Dsbt.color=always -Dsbt.supershell=false ";jvm/publishLocal;jvm/publish"
+        sbt -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION  -Dsbt.color=always -Dsbt.supershell=false ";izanami-server/assembly;izanami-server/dist;izanami-server/docker:publish;"
+        sbt -J-Xmx2G -J-Xss20M -J-XX:ReservedCodeCacheSize=128m ++$TRAVIS_SCALA_VERSION  -Dsbt.color=always -Dsbt.supershell=false ";jvm/publishLocal;jvm/publish;izanami-spring/publish"
 #        echo "Uploading izanami.jar"
 #        curl -T ./izanami-server/target/scala-2.12/izanami.jar -u${BINTRAY_USER}:${BINTRAY_PASS} -H 'X-Bintray-Publish: 1' -H 'X-Bintray-Override: 1' -H 'X-Bintray-Version: latest' -H 'X-Bintray-Package: izanami.jar' https://api.bintray.com/content/maif/binaries/izanami.jar/latest/izanami.jar
 #        curl -T ./izanami-server/target/universal/izanami.zip -u${BINTRAY_USER}:${BINTRAY_PASS} -H 'X-Bintray-Publish: 1' -H 'X-Bintray-Override: 1' -H 'X-Bintray-Version: latest' -H 'X-Bintray-Package: izanami-dist' https://api.bintray.com/content/maif/binaries/izanami-dist/latest/izanami-dist.zip
