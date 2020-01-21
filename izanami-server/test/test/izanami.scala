9ad185b72b76fa183f07fe722ada8202b51fca49
larousso <adelegue@hotmail.com>
2020-01-21 14:35:19 +0100

    :sparkles: Fix some tests

--- izanami-server/test/test/izanami.scala
+++ izanami-server/test/test/izanami.scala
@@ -132,7 +132,7 @@ trait OneServerPerSuiteWithMyComponents
     with ScalaFutures
     with AddConfiguration { this: TestSuite =>
 
-  def user =
+  def user: User =
     IzanamiUser(id = "id",
                 name = "Ragnar Lodbrok",
                 email = "ragnar.lodbrok@gmail.com",
