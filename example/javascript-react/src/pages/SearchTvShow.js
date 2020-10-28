b8862661c1f94c077b3b54793a251c122fc90000
pierrebrunin <pierre.brunin@maif.fr>
2020-10-28 17:21:06 +0100

    :arrow_up: prop-types => 15.7.2 with the lib dependent (react-select, etc) of prop-types link example with local version of izanami-client

--- example/javascript-react/src/pages/SearchTvShow.js
+++ example/javascript-react/src/pages/SearchTvShow.js
@@ -1,53 +1,7 @@
 import React from 'react';
-import Select from 'react-select';
+import Select from 'react-select/async';
 import _ from "lodash";
-import 'react-select/dist/react-select.css';
-import PropTypes from 'prop-types';
 import * as Service from '../services';
-
-class TvShowOption extends React.Component {
-  static propTypes = {
-    children: PropTypes.node,
-    className: PropTypes.string,
-    isDisabled: PropTypes.bool,
-    isFocused: PropTypes.bool,
-    isSelected: PropTypes.bool,
-    onFocus: PropTypes.func,
-    onSelect: PropTypes.func,
-    option: PropTypes.object.isRequired,
-  };
-
-  onSelect = event => {
-    event.preventDefault();
-    event.stopPropagation();
-    this.props.onSelect(this.props.option, event);
-    Service.addTvShow(this.props.option.id)
-  };
-
-  render() {
-    const {option:{image, title, description, source}} = this.props;
-    return (
-      <div>
-        <button className={"btn btn-default btnSearch"} style={{width:'100%'}} onClick={this.onSelect}>
-        <div className="row resultSearch">
-            <div className="col-md-3">
-              {image && <img width="300px" src={`${image}`} />}
-            </div>
-            <div className="col-md-9">
-              <div className="TvResult">
-                <h3>{title} ({source})</h3>
-                <p>
-                {_.truncate(description, {'length':120})}
-                </p>
-              </div>
-            </div>
-          </div>
-        </button>
-      </div>
-    );
-  }
-
-}
 
 export default class SearchTvShow extends React.Component {
 
@@ -57,31 +11,40 @@ export default class SearchTvShow extends React.Component {
 
   getOptions = (input) => {
     return Service.searchTvShow(input)
-      .then((options) => {
-        return {options};
-      });
-  };
-
-  setValue = (value) => {
-  };
-
-  selectValue = (value, event) => {
-    console.log("Selected", value);
-    this.setState({ value });
   };
 
   render() {
-    return <Select.Async
+    const Option = (props) => {
+      if(props.isDisabled) return null
+      return (
+          <div className="row resultSearch">
+            <button className={"btn btn-default btnSearch"} style={{width: '100%'}} onClick={() => props.setValue(props.data)}>
+            <div className="col-md-3">
+              {props.data.image && <img width="300px" src={`${props.data.image}`}/>}
+            </div>
+            <div className="col-md-9">
+              <div className="TvResult">
+                <h3>{props.data.title} ({props.data.source})</h3>
+                <p>
+                  {_.truncate(props.data.description, {'length': 120})}
+                </p>
+              </div>
+            </div>
+            </button>
+          </div>
+      )
+    }
+    return <Select
       name="form-field-name"
-      onChange={this.setValue}
-      onValueClick={this.selectValue}
-      value={this.state.value}
-      valueKey="id" labelKey="title"
+      onChange={value => {
+        Service.addTvShow(value.id).then(() => this.setState({value}))
+      }}
+      inputValue={this.state.inputValue}
+      blurInputOnSelect={false}
+      onInputChange={inputValue => this.setState({ inputValue })}
       loadOptions={this.getOptions}
       placeholder={"Search a tv show"}
-      optionComponent={TvShowOption}
+      components={{Option}}
     />
   }
-
-
 }
