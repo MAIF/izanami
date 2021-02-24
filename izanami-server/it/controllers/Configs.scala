a1646bd312776af00a12b5a36898ce0c2142c7f2
pierrebrunin <pierre.brunin@maif.fr>
2021-02-24 17:02:55 +0100

    Add ES 7 compatibility (#511)

--- izanami-server/it/controllers/Configs.scala
+++ izanami-server/it/controllers/Configs.scala
@@ -5,7 +5,6 @@ import java.util.concurrent.atomic.AtomicInteger
 
 import akka.testkit.SocketUtil._
 import com.typesafe.config.ConfigFactory
-import elastic.client.ElasticClient
 import libs.IdGenerator
 import org.iq80.leveldb.util.FileUtils
 import org.scalatest._
@@ -27,41 +26,48 @@ object Configs {
 
   val cassandraPort: Int = 9042
 
-  val elasticHttpPort: Int = 9210
+  val elastic6HttpPort: Int = 9210
 
-  def elasticConfiguration: Configuration = Configuration(
+  val elastic7HttpPort: Int = 9220
+
+  def elastic6Configuration: Configuration = elasticConfiguration(elastic6HttpPort, 6)
+
+  def elastic7Configuration: Configuration = elasticConfiguration(elastic7HttpPort, 7)
+
+  def elasticConfiguration(elasticHttpPort: Int, version: Int): Configuration = Configuration(
     ConfigFactory.parseString(s"""
-      |izanami.db.default="Elastic"
-      |izanami.patchEnabled = false
-      |izanami.mode= "test"
-      |izanami.namespace="izanami-${Random.nextInt(1000)}"
-      |izanami.config.db.type=$${izanami.db.default}
-      |izanami.config.db.conf.namespace="izanami-${Random.nextInt(1000)}:configuration"
-      |izanami.features.db.type=$${izanami.db.default}
-      |izanami.features.db.conf.namespace="izanami-${Random.nextInt(1000)}:feature"
-      |izanami.globalScript.db.type=$${izanami.db.default}
-      |izanami.globalScript.db.conf.namespace="izanami-${Random.nextInt(1000)}:script"
-      |izanami.experiment.db.type=$${izanami.db.default}
-      |izanami.experiment.db.conf.namespace="izanami-${Random.nextInt(1000)}:experiment"
-      |izanami.experimentEvent.db.type=$${izanami.db.default}
-      |izanami.experimentEvent.db.conf.namespace="izanami-${Random.nextInt(1000)}:events"
-      |izanami.webhook.db.type=$${izanami.db.default}
-      |izanami.webhook.db.conf.namespace="izanami-${Random.nextInt(1000)}:hooks"
-      |izanami.user.db.type=$${izanami.db.default}
-      |izanami.user.db.conf.namespace="izanami-${Random.nextInt(1000)}:user"
-      |izanami.apikey.db.type=$${izanami.db.default}
-      |izanami.apikey.db.conf.namespace="izanami-${Random.nextInt(1000)}:apikey"
-      |izanami.patch.db.type=$${izanami.db.default}
-      |
-      |izanami {
-      |  db {
-      |    elastic {
-      |      host = "localhost"
-      |      port = $elasticHttpPort
-      |      automaticRefresh = true
-      |    }
-      |  }
-      |}
+                                 |izanami.db.default="Elastic"
+                                 |izanami.patchEnabled = false
+                                 |izanami.mode= "test"
+                                 |izanami.namespace="izanami-${Random.nextInt(1000)}"
+                                 |izanami.config.db.type=$${izanami.db.default}
+                                 |izanami.config.db.conf.namespace="izanami-${Random.nextInt(1000)}:configuration"
+                                 |izanami.features.db.type=$${izanami.db.default}
+                                 |izanami.features.db.conf.namespace="izanami-${Random.nextInt(1000)}:feature"
+                                 |izanami.globalScript.db.type=$${izanami.db.default}
+                                 |izanami.globalScript.db.conf.namespace="izanami-${Random.nextInt(1000)}:script"
+                                 |izanami.experiment.db.type=$${izanami.db.default}
+                                 |izanami.experiment.db.conf.namespace="izanami-${Random.nextInt(1000)}:experiment"
+                                 |izanami.experimentEvent.db.type=$${izanami.db.default}
+                                 |izanami.experimentEvent.db.conf.namespace="izanami-${Random.nextInt(1000)}:events"
+                                 |izanami.webhook.db.type=$${izanami.db.default}
+                                 |izanami.webhook.db.conf.namespace="izanami-${Random.nextInt(1000)}:hooks"
+                                 |izanami.user.db.type=$${izanami.db.default}
+                                 |izanami.user.db.conf.namespace="izanami-${Random.nextInt(1000)}:user"
+                                 |izanami.apikey.db.type=$${izanami.db.default}
+                                 |izanami.apikey.db.conf.namespace="izanami-${Random.nextInt(1000)}:apikey"
+                                 |izanami.patch.db.type=$${izanami.db.default}
+                                 |
+                                 |izanami {
+                                 |  db {
+                                 |    elastic {
+                                 |      host = "localhost"
+                                 |      port = $elasticHttpPort
+                                 |      version = $version
+                                 |      automaticRefresh = true
+                                 |    }
+                                 |  }
+                                 |}
       """.stripMargin).resolve()
   )
 
@@ -316,20 +322,43 @@ object Configs {
     FileUtils.deleteRecursively(new File("./target/leveldb/"))
   }
 
-  def initEs = if (Try(Option(System.getenv("CI"))).toOption.flatten.exists(!_.isEmpty)) {
-    import elastic.codec.PlayJson._
-    val client: ElasticClient[JsValue] = ElasticClient[JsValue](port = Configs.elasticHttpPort)
+  def initEs6 = if (Try(Option(System.getenv("CI"))).toOption.flatten.exists(!_.isEmpty)) {
+    import elastic.es6.codec.PlayJson._
+    import elastic.es6.client.{ElasticClient => ElasticClient6}
+    val client: ElasticClient6[JsValue] = ElasticClient6[JsValue](port = elastic6HttpPort)
     println("Cleaning ES indices")
     Await.result(client.deleteIndex("*"), 5.seconds)
     Await.result(
       client.put(
         Path.Empty / "_cluster" / "settings",
         Some("""
-        |{
-        |    "persistent" : {
-        |        "cluster.routing.allocation.disk.threshold_enabled" : false
-        |    }
-        |}
+               |{
+               |    "persistent" : {
+               |        "cluster.routing.allocation.disk.threshold_enabled" : false
+               |    }
+               |}
+      """.stripMargin)
+      ),
+      5.seconds
+    )
+    Thread.sleep(1000)
+  }
+
+  def initEs7 = if (Try(Option(System.getenv("CI"))).toOption.flatten.exists(!_.isEmpty)) {
+    import elastic.es7.codec.PlayJson._
+    import elastic.es7.client.{ElasticClient => ElasticClient7}
+    val client: ElasticClient7[JsValue] = ElasticClient7[JsValue](port = elastic7HttpPort)
+    println("Cleaning ES indices")
+    Await.result(client.deleteIndex("*"), 5.seconds)
+    Await.result(
+      client.put(
+        Path.Empty / "_cluster" / "settings",
+        Some("""
+               |{
+               |    "persistent" : {
+               |        "cluster.routing.allocation.disk.threshold_enabled" : false
+               |    }
+               |}
       """.stripMargin)
       ),
       5.seconds
