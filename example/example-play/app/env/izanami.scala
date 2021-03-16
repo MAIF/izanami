74e3f5c5357bb9a2de152a5887c11fd004cc8a82
pierrebrunin <pierre.brunin@maif.fr>
2021-03-16 15:48:26 +0100

    Add header config in play example

--- example/example-play/app/env/izanami.scala
+++ example/example-play/app/env/izanami.scala
@@ -6,11 +6,25 @@ import izanami.scaladsl.IzanamiClient
 object Izanami {
 
   def izanamiConfig(izanamiConf: IzanamiConf): ClientConfig =
-    ClientConfig(
-      host = izanamiConf.host,
-      clientId = izanamiConf.clientId,
-      clientSecret = izanamiConf.clientSecret,
-      backend = IzanamiBackend.SseBackend
-    )
+    (for {
+      clientIdHeaderName     <- izanamiConf.clientIdHeaderName
+      clientSecretHeaderName <- izanamiConf.clientSecretHeaderName
+    } yield {
+      ClientConfig(
+        host = izanamiConf.host,
+        clientId = izanamiConf.clientId,
+        clientSecret = izanamiConf.clientSecret,
+        clientIdHeaderName = clientIdHeaderName,
+        clientSecretHeaderName = clientSecretHeaderName,
+        backend = IzanamiBackend.SseBackend
+      )
+    }).getOrElse {
+      ClientConfig(
+        host = izanamiConf.host,
+        clientId = izanamiConf.clientId,
+        clientSecret = izanamiConf.clientSecret,
+        backend = IzanamiBackend.SseBackend
+      )
+    }
 
 }
