ba64146b8d7d81e9aa9b2aa3cfec70934c7bf3b8
pierrebrunin <pierre.brunin@maif.fr>
2021-03-24 17:36:49 +0100

    Re activate CI for izanami-cli Switch bintray to github release for izanami-cli

--- scripts/build-rust.sh
+++ scripts/build-rust.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-
 LOCATION=`pwd`
 
-cd $LOCATION/izanami-clients/izanami-cli
-cargo clean
-cargo build --release
-
-BRANCH_NAME=`git branch --show-current`
 OSINFO=`uname -a`
 
 if [[ $OSINFO != Linux* ]];
@@ -16,23 +10,11 @@ then
   exit 1
 fi
 
-OS_NAME="linux"
+cd $LOCATION/izanami-clients/izanami-cli
+cargo clean
+cargo build --release
+#
+#rustup target add x86_64-apple-darwin
+#cargo build --release --target=x86_64-apple-darwin
 
-if [ -n "$BRANCH_NAME" ];
-then
-    if test "$BRANCH_NAME" = "master"
-    then
-        CLI_VERSION="latest"
-        echo "Releasing rust with version: ${CLI_VERSION}"
-
-        curl -T ./target/release/izanami-cli -u${BINTRAY_USER}:${BINTRAY_PASS} -H "X-Bintray-Publish: 1" -H "X-Bintray-Override: 1" -H "X-Bintray-Version: ${CLI_VERSION}" -H "X-Bintray-Package: ${OS_NAME}-izanamicli" https://api.bintray.com/content/maif/binaries/${OS_NAME}-izanamicli/${CLI_VERSION}/izanami-cli
-    fi
-else
-    CLI_VERSION="${BINARIES_VERSION}"
-    echo "Releasing rust with version: ${CLI_VERSION}"
-
-    curl -T ./target/release/izanami-cli -u${BINTRAY_USER}:${BINTRAY_PASS} -H "X-Bintray-Publish: 1" -H "X-Bintray-Override: 1" -H "X-Bintray-Version: ${CLI_VERSION}" -H "X-Bintray-Package: ${OS_NAME}-izanamicli" https://api.bintray.com/content/maif/binaries/${OS_NAME}-izanamicli/${CLI_VERSION}/izanami-cli
-
-    cd $LOCATION
-fi
 
