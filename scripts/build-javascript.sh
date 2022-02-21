9de5bf0912c297fef6851c9897f6a9c7394cff61
pierrebrunin <pierre.brunin@maif.fr>
2022-02-21 12:59:29 +0100

    build frontend with yarn

--- scripts/build-javascript.sh
+++ scripts/build-javascript.sh
@@ -4,9 +4,5 @@ LOCATION=`pwd`
 
 cd $LOCATION/izanami-server/javascript
 
-npm config set scripts-prepend-node-path true
-
-#yarn install
-#yarn build
-npm install --no-optional
-npm run build
+yarn install
+yarn build
