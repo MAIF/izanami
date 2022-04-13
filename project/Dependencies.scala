b28d113f033935a351aeff306f2b2bd60b70b718
pierrebrunin <pierre.brunin@maif.fr>
2022-04-13 13:54:43 +0200

    fix critical vuls (#649)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 object Dependencies {
   val scalaVersion = "2.13.3"
 
-  val akkaVersion     = "2.6.18"
+  val akkaVersion     = "2.6.19"
   val akkaHttpVersion = "10.1.15"
   val alpakkaVersion  = "1.1.2"
 }
