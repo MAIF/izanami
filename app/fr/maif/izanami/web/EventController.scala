d85ce81b6275fdd693a892a80989fbf13361b113
Benjamin Cavy <benjamin.cavy@gmail.com>
2024-03-27 01:24:52 +0100

    WIP

--- app/fr/maif/izanami/web/EventController.scala
+++ app/fr/maif/izanami/web/EventController.scala
@@ -3,16 +3,16 @@ package fr.maif.izanami.web
 import akka.NotUsed
 import akka.stream.Materializer
 import akka.stream.scaladsl.{BroadcastHub, Flow, Keep, Source}
-import fr.maif.izanami.datastores.{FeatureCreated, FeatureDeleted, FeatureUpdated}
+import fr.maif.izanami.datastores.{EventType, FeatureCreated, FeatureDeleted, FeatureUpdated}
 import fr.maif.izanami.env.Env
-import fr.maif.izanami.errors.FailedToReadEvent
-import fr.maif.izanami.models.Feature
+import fr.maif.izanami.errors.{FailedToReadEvent, IzanamiError}
+import fr.maif.izanami.models.{AbstractFeature, Feature, FeatureRequest, RequestContext}
 import fr.maif.izanami.v1.V1FeatureEvents.{createEvent, deleteEvent, keepAliveEvent, updateEvent}
 import io.vertx.pgclient.pubsub.PgSubscriber
 import play.api.http.ContentTypes
 import play.api.libs.EventSource
 import play.api.libs.EventSource.{EventDataExtractor, EventIdExtractor, EventNameExtractor}
-import play.api.libs.json.{JsObject, Json}
+import play.api.libs.json.{JsObject, JsValue, Json}
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 
 import scala.concurrent.duration.DurationInt
@@ -22,11 +22,11 @@ import scala.util.{Failure, Success}
 class EventController(val controllerComponents: ControllerComponents, val clientKeyAction: ClientApiKeyAction)(implicit
     val env: Env
 ) extends BaseController {
-  implicit val ec: ExecutionContext       = env.executionContext;
-  implicit val materializer: Materializer = env.materializer
-  val sources:scala.collection.mutable.Map[String, Source[InternalEvent, NotUsed]] = scala.collection.mutable.Map()
+  implicit val ec: ExecutionContext                                                = env.executionContext;
+  implicit val materializer: Materializer                                          = env.materializer
+  val sources: scala.collection.mutable.Map[String, Source[GenericEvent, NotUsed]] = scala.collection.mutable.Map()
 
-  val logger                              = env.logger
+  val logger                                                       = env.logger
   // FIXME create dedicated object instead
   private implicit val nameExtractor: EventNameExtractor[JsObject] =
     EventNameExtractor[JsObject](_ => None) //Some(event.`type`))
@@ -36,14 +36,14 @@ class EventController(val controllerComponents: ControllerComponents, val client
   private implicit val dataExtractor: EventDataExtractor[JsObject] =
     EventDataExtractor[JsObject](event => Json.stringify(event))
 
+  case class GenericEvent(eventType: EventType, project: String, maybeFeature: Option[AbstractFeature], id: String)
   case class InternalEvent(id: String, project: String, payload: JsObject)
 
-  private def eventSource(tenant: String): Source[InternalEvent, NotUsed] = {
+  private def eventSource(tenant: String): Source[GenericEvent, NotUsed] = {
     lazy val (queue, source) = Source
-      .queue[InternalEvent](bufferSize = 1024)
+      .queue[GenericEvent](bufferSize = 1024)
       .toMat(BroadcastHub.sink(bufferSize = 1024))(Keep.both)
       .run()
-
 
     lazy val subscriber = PgSubscriber.subscriber(env.postgresql.vertx, env.postgresql.connectOptions)
     subscriber
@@ -53,41 +53,44 @@ class EventController(val controllerComponents: ControllerComponents, val client
           subscriber
             .channel(s"${tenant}-features")
             .handler(payload => {
-              val json = Json.parse(payload);
-
+              val json            = Json.parse(payload);
               val maybeFuturEvent =
                 for (
-                  id <- (json \ "id").asOpt[String];
-                  project <- (json \ "project").asOpt[String];
+                  id        <- (json \ "id").asOpt[String];
+                  project   <- (json \ "project").asOpt[String];
                   eventType <- (json \ "type").asOpt[String]
                 )
-                yield {
-                  val futureEitherEvent = if (eventType.equalsIgnoreCase(FeatureUpdated.toString) || eventType.equalsIgnoreCase(FeatureCreated.toString)) {
-                    env.datastores.features.findById(tenant, id).map {
-                      case Left(value) => Left(value)
-                      case Right(Some(feature)) => Feature.writeFeatureInLegacyFormat(feature) match {
-                        case Some(json) if eventType.equalsIgnoreCase(FeatureUpdated.toString) => Right(Some(updateEvent(id, json)))
-                        case Some(json) if eventType.equalsIgnoreCase(FeatureCreated.toString) => Right(Some(createEvent(id, json)))
-                        case _ => Right(None)
-                      }
-                      case Right(None) => Right(Some(deleteEvent(id))) // Feature has been deleted since event was emitted
+                  yield {
+                    val formalType: EventType = eventType match {
+                      case eventType if eventType.equalsIgnoreCase(FeatureUpdated.toString) => FeatureUpdated
+                      case eventType if eventType.equalsIgnoreCase(FeatureCreated.toString) => FeatureCreated
+                      case eventType if eventType.equalsIgnoreCase(FeatureDeleted.toString) => FeatureDeleted
                     }
-                  } else if (eventType.equalsIgnoreCase(FeatureDeleted.toString)) {
-                    Future.successful(Right(Some(deleteEvent(id))))
-                  } else {
-                    Future
-                      .successful(Left(FailedToReadEvent(payload)))
+
+                    formalType match {
+                      case f @ (FeatureUpdated | FeatureCreated) =>
+                        env.datastores.features
+                          .findById(tenant, id)
+                          .map(either => {
+                            either.map(maybeFeature =>
+                              GenericEvent(
+                                eventType = formalType,
+                                project = project,
+                                maybeFeature = maybeFeature,
+                                id = id
+                              )
+                            )
+                          })
+                      case f @ FeatureDeleted                    =>
+                        Future.successful(
+                          Right(GenericEvent(eventType = formalType, project = project, maybeFeature = None, id = id))
+                        )
+                    }
                   }
 
-                  futureEitherEvent.map {
-                    case Right(Some(json)) => Right(Some(InternalEvent(id=id, project=project, payload=json)))
-                    case Right(None) => Right(None)
-                    case Left(err) => Left(err)
-                  }
-                }
               maybeFuturEvent.getOrElse(Future.successful(Left(FailedToReadEvent(payload)))).map {
-                case Left(error) => env.logger.error(s"Failed to process event : ${error.message}")
-                case Right(value) => value.foreach(value => queue.offer((value)))
+                case Left(error)  => env.logger.error(s"Failed to process event : ${error.message}")
+                case Right(value) => queue.offer(value)
               }
             })
         } else {
@@ -99,6 +102,21 @@ class EventController(val controllerComponents: ControllerComponents, val client
     source
   }
 
+  def processForLegacyEndpoint(event: GenericEvent): JsObject = {
+    if (event.eventType == FeatureDeleted) {
+      deleteEvent(event.id)
+    } else {
+      event.maybeFeature.fold(deleteEvent(event.id))(f => {
+        val legacyFormatFeature = Feature.writeFeatureInLegacyFormat(f)
+        if (event.eventType == FeatureCreated) {
+          createEvent(event.id, legacyFormatFeature)
+        } else {
+          updateEvent(event.id, legacyFormatFeature)
+        }
+      })
+    }
+  }
+
   val keepAlive: Flow[JsObject, JsObject, NotUsed] = Flow[JsObject].keepAlive(
     30.seconds,
     () => {
@@ -106,32 +124,91 @@ class EventController(val controllerComponents: ControllerComponents, val client
     }
   )
 
+  def processForModernEndpoint(event: GenericEvent, context: RequestContext, env: Env): Future[Option[JsObject]] = {
+    if (event.eventType == FeatureDeleted) {
+      Future.successful(Some(deleteEvent(event.id)))
+    } else {
+      event.maybeFeature.fold(Future.successful(Some(deleteEvent(event.id)): Option[JsObject]))(f => {
+        Feature.writeFeatureForCheck(f, context, env).map {
+          case Left(error) => {
+            logger.error(s"Failed to write feature : ${error.message}")
+            None
+          }
+          case Right(json) => {
+            if (event.eventType == FeatureCreated) {
+              Some(createEvent(event.id, json))
+            } else {
+              Some(updateEvent(event.id, json))
+            }
+          }
+        }
+      })
+    }
+  }
+
   def events(pattern: String, domains: String): Action[AnyContent] = clientKeyAction.async { request =>
     val key = request.key
 
     val regexpPattern = pattern.replaceAll("\\*", ".*")
-    val tenant          = key.tenant
+    val tenant        = key.tenant
 
-    val source = sources.getOrElseUpdate(tenant,  eventSource(tenant))
+    val source = sources.getOrElseUpdate(tenant, eventSource(tenant))
 
-    val resultSource = source.filter{case InternalEvent(id, project, _) =>
-      id.matches(regexpPattern) && (key.admin || key.projects.exists(ap => ap.name == project))
-    }.map(_.payload)
+    val resultSource = source
+      .filter { case GenericEvent(_, project, _, id) =>
+        id.matches(regexpPattern) && (key.admin || key.projects.exists(ap => ap.name == project))
+      }
+      .map(e => processForLegacyEndpoint(e))
 
-    val s               = resultSource via keepAlive via EventSource.flow
+    val s = resultSource via keepAlive via EventSource.flow
     Future.successful(
       Ok.chunked(
         s.watchTermination()((_, future) =>
           future.onComplete {
             case Failure(exception) => logger.error("Event source failed", exception)
             case Success(_)         => {
-              logger.debug("Event source closed, closing postgres subscriber")
-              // TODO how to close parent source ?
+              logger.debug("Event source closed")
             }
           }
         )
       ).as(ContentTypes.EVENT_STREAM)
     )
+  }
+
+  def newEvents(user: String, clientRequest: FeatureRequest): Action[AnyContent] = clientKeyAction.async { request =>
+    val key    = request.key
+    val tenant = key.tenant
+
+    val source = sources.getOrElseUpdate(tenant, eventSource(tenant))
+    env.datastores.projects
+      .readProjectsById(tenant, clientRequest.projects)
+      .map(m => m.values.map(p => p.name).toSet)
+      .map(allowedProjects => {
+        val resultSource = source
+          .filter { case GenericEvent(_, project, _, id) =>
+            key.admin || key.projects.exists(ap => ap.name == project)
+          }
+          .filter { case GenericEvent(_, project, _, id) =>
+            // TODO handle tags
+            allowedProjects.contains(project) ||
+              clientRequest.features.contains(id)
+          }
+          .mapAsync(1)(e => processForModernEndpoint(e, RequestContext(tenant, user, FeatureContextPath(elements = clientRequest.context)), env))
+          .filter(_.isDefined)
+          .map(_.get)
+
+        val s = resultSource via keepAlive via EventSource.flow
+        Ok.chunked(
+          s.watchTermination()((_, future) =>
+            future.onComplete {
+              case Failure(exception) => logger.error("Event source failed", exception)
+              case Success(_) => {
+                logger.debug("Event source closed")
+              }
+            }
+          )
+        ).as(ContentTypes.EVENT_STREAM)
+      })
   }
 
 }
