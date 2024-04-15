bd1865d3642ac252384bd93af758a4f224e49275
Benjamin Cavy <benjamin.cavy@gmail.com>
2024-04-15 11:09:54 +0200

    fix: center new project button on empty tenant page

--- izanami-frontend/src/pages/tenant.tsx
+++ izanami-frontend/src/pages/tenant.tsx
@@ -67,6 +67,34 @@ function ProjectList(props: { tenant: TenantType }) {
           </button>
         )}
       </div>
+      {noProjects && !creating && (
+        <div className="item-block">
+          <div className="item-text">
+            This tenant does not have any project yet.
+          </div>
+          <button
+            type="button"
+            className="btn btn-primary btn-lg"
+            style={{
+              marginBottom: "12px",
+              width: "215px",
+            }}
+            onClick={() => setCreating(true)}
+          >
+            Create new project
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary btn-lg"
+            style={{
+              width: "215px",
+            }}
+            onClick={() => navigate(`/tenants/${tenant.name}/settings`)}
+          >
+            Import data
+          </button>
+        </div>
+      )}
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 nav mt-4">
         {creating && (
           <div className="col" key="new-project">
@@ -104,34 +132,7 @@ function ProjectList(props: { tenant: TenantType }) {
             </div>
           </div>
         )}
-        {noProjects && !creating && (
-          <div className="item-block">
-            <div className="item-text">
-              This tenant does not have any project yet.
-            </div>
-            <button
-              type="button"
-              className="btn btn-primary btn-lg"
-              style={{
-                marginBottom: "12px",
-                width: "215px",
-              }}
-              onClick={() => setCreating(true)}
-            >
-              Create new project
-            </button>
-            <button
-              type="button"
-              className="btn btn-secondary btn-lg"
-              style={{
-                width: "215px",
-              }}
-              onClick={() => navigate(`/tenants/${tenant.name}/settings`)}
-            >
-              Import data
-            </button>
-          </div>
-        )}
+
         {tenant?.projects?.map(({ name: pName, description }) => (
           <div className="col" key={pName}>
             <div className="card shadow-sm">
