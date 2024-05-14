4aca7ef03d685727550f84c14018aad11768d935
HelaKaraa <hela.karaa@gmail.com>
2024-05-14 17:40:35 +0200

    feat : ajout indeterminate state in select tags

--- izanami-frontend/src/components/MultiSelect.tsx
+++ izanami-frontend/src/components/MultiSelect.tsx
@@ -5,7 +5,8 @@ import { customStyles } from "../styles/reactSelect";
 export type Option = {
   value: string;
   label: string;
-  state: boolean;
+  checked: boolean;
+  indeterminate: boolean;
 };
 
 interface ISelectProps {
@@ -15,21 +16,55 @@ interface ISelectProps {
   defaultValue: Option[];
   labelBy: string;
 }
+const CHECKBOX_STATES = {
+  Checked: "Checked",
+  Indeterminate: "Indeterminate",
+  Empty: "Empty",
+};
 
 const MultiSelect = (props: ISelectProps) => {
   const { options, value, defaultValue, onSelected, labelBy } = props;
 
   const Option = (props: any) => {
+    const { isSelected, data } = props;
+    const [state, setState] = React.useState(
+      data.indeterminate && isSelected
+        ? CHECKBOX_STATES.Indeterminate
+        : isSelected && !data.indeterminate
+        ? CHECKBOX_STATES.Checked
+        : CHECKBOX_STATES.Empty
+    );
+
+    const handleChange = () => {
+      let updatedChecked =CHECKBOX_STATES.Empty ;
+      if (state === CHECKBOX_STATES.Checked) {
+        updatedChecked = CHECKBOX_STATES.Empty;
+      } else if (state === CHECKBOX_STATES.Empty) {
+        updatedChecked = CHECKBOX_STATES.Checked;
+      } else if (state === CHECKBOX_STATES.Indeterminate) {
+        updatedChecked = CHECKBOX_STATES.Checked;
+      }
+      setState(updatedChecked);
+    };
+
+    const checkboxRef = React.useRef<HTMLInputElement>(null!);
+
+    React.useEffect(() => {
+      checkboxRef.current.checked = state === CHECKBOX_STATES.Checked;
+      checkboxRef.current.indeterminate =
+        state === CHECKBOX_STATES.Indeterminate;
+    }, [state]);
+
     return (
       <components.Option {...props}>
         <input
           type="checkbox"
-          checked={props.isSelected}
-          onChange={() => null}
+          onChange={handleChange}
+          ref={checkboxRef}
           style={{
-            accentColor: props.isSelected ? "#dc5f9f" : "#fff",
+            accentColor: state !== CHECKBOX_STATES.Empty ? "#dc5f9f" : "#fff",
           }}
-        />{" "}
+        />
         <label style={{ marginLeft: "5px" }}>{props.label}</label>
       </components.Option>
     );
