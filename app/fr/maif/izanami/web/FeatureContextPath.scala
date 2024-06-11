33ab86c3a8b678fbdb0b73510de5bb495b1b9721
Benjamin Cavy <benjamin.cavy@gmail.com>
2024-06-11 14:53:48 +0200

    fix: support starting "/" for contexts

--- app/fr/maif/izanami/web/FeatureContextPath.scala
+++ app/fr/maif/izanami/web/FeatureContextPath.scala
@@ -2,31 +2,33 @@ package fr.maif.izanami.web
 
 import play.api.mvc.{PathBindable, QueryStringBindable}
 
-case class FeatureContextPath(elements: Seq[String]=Seq())
-
+case class FeatureContextPath(elements: Seq[String] = Seq())
 
 object FeatureContextPath {
-  implicit def pathBinder(implicit strBinder: PathBindable[String]): PathBindable[FeatureContextPath] = new PathBindable[FeatureContextPath] {
-    override def bind(key: String, value: String): Either[String, FeatureContextPath] = {
-      strBinder.bind(key, value).map(str => {
-        FeatureContextPath(str.split("/").toSeq)
-      })
+  implicit def pathBinder(implicit strBinder: PathBindable[String]): PathBindable[FeatureContextPath] =
+    new PathBindable[FeatureContextPath] {
+      override def bind(key: String, value: String): Either[String, FeatureContextPath] = {
+        strBinder
+          .bind(key, value)
+          .map(str => {
+            FeatureContextPath(str.split("/").toSeq.filter(s => s.nonEmpty))
+          })
+      }
+      override def unbind(key: String, path: FeatureContextPath): String = {
+        path.elements.mkString("/")
+      }
     }
-    override def unbind(key: String, path: FeatureContextPath): String = {
-      path.elements.mkString("/")
-    }
-  }
 
   implicit def queryStringBindable(implicit
-                                   seqBinder: QueryStringBindable[Seq[String]]
-                                  ): QueryStringBindable[FeatureContextPath] =
+      seqBinder: QueryStringBindable[Seq[String]]
+  ): QueryStringBindable[FeatureContextPath] =
     new QueryStringBindable[FeatureContextPath] {
       override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, FeatureContextPath]] = {
-        for (
-          eitherContext <- seqBinder.bind("context", params)
-        ) yield {
+        for (eitherContext <- seqBinder.bind("context", params)) yield {
           Right(
-            FeatureContextPath(elements=eitherContext.map(seq => seq.filter(str => str.nonEmpty).flatMap(str => str.split("/"))).getOrElse(Seq()))
+            FeatureContextPath(elements =
+              eitherContext.map(seq => seq.filter(str => str.nonEmpty).flatMap(str => str.split("/").toSeq.filter(s => s.nonEmpty))).getOrElse(Seq())
+            )
           )
         }
       }
@@ -36,4 +38,3 @@ object FeatureContextPath {
       }
     }
 }
-
