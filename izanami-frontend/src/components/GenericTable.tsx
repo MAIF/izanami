b3af4dc323d2cb0bcc60c807d27e05c680417a10
baudelotphilippe <philippe@baudelot.eu>
2024-06-13 11:35:30 +0200

    remove ugly scrollbar

--- izanami-frontend/src/components/GenericTable.tsx
+++ izanami-frontend/src/components/GenericTable.tsx
@@ -269,7 +269,7 @@ export function GenericTable<T extends RowData>(props: TProps<T>) {
   }, [rowSelection]);
 
   return (
-    <div className="overflow-scroll">
+    <div>
       <table className="table table-borderless table-striped mt-2">
         <thead>
           {table.getHeaderGroups().map((headerGroup) => (
