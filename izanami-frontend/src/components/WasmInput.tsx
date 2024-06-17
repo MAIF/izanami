b4e509d3d964c478d8eee3017ef8b58363c91f52
HelaKaraa <hela.karaa@gmail.com>
2024-06-17 18:03:15 +0200

    fix#769 : Rebased to main

--- izanami-frontend/src/components/WasmInput.tsx
+++ izanami-frontend/src/components/WasmInput.tsx
@@ -275,6 +275,7 @@ function loadWasmManagerScripts() {
   })
     .then((r) => r.json())
     .then((plugins) => {
+      console.log("plugins", plugins);
       return (
         plugins
           .map(
@@ -286,6 +287,7 @@ function loadWasmManagerScripts() {
           .flat()
           // FIXME TS
           .map((plugin: any) => {
+            console.log("plugin", plugin);
             const wasmName = (isAString(plugin) ? plugin : plugin.name) || "";
             const parts = wasmName.split(".wasm");
             return {
