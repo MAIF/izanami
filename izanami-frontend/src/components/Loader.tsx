d7f6270ccf3c9f99b20a935e3122edf87627bd0d
Benjamin Cavy <benjamin.cavy@gmail.com>
2024-06-22 15:46:20 +0200

    feat: loading indicator for tenant creation form

--- izanami-frontend/src/components/Loader.tsx
+++ izanami-frontend/src/components/Loader.tsx
@@ -1,14 +1,16 @@
 import * as React from "react";
 
-export function Loader(props: { message: string }) {
+export function Loader(props: { message?: string }) {
   return (
     <>
       <div className="d-flex justify-content-center">
         <i className="fas fa-cog fa-spin" style={{ fontSize: "30px" }} />
       </div>
-      <h5 style={{ textAlign: "center" }} className="mt-3">
-        {props.message}
-      </h5>
+      {props.message && (
+        <h5 style={{ textAlign: "center" }} className="mt-3">
+          {props.message}
+        </h5>
+      )}
     </>
   );
 }
