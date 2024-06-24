0ac7e925830703b8c32e711bc5892f7c4ccc7d84
Benjamin Cavy <benjamin.cavy@gmail.com>
2024-06-24 15:22:30 +0200

    feat: better forms

--- izanami-frontend/src/components/InvitationForm.tsx
+++ izanami-frontend/src/components/InvitationForm.tsx
@@ -3,6 +3,8 @@ import { TLevel } from "../utils/types";
 import AsyncSelect from "react-select/async";
 import Select from "react-select";
 import { customStyles } from "../styles/reactSelect";
+import { Controller, FormProvider, useForm } from "react-hook-form";
+import { ErrorMessage } from "../components/HookFormErrorMessage";
 
 const loadOptions = (
   inputValue: string,
@@ -15,59 +17,94 @@ const loadOptions = (
     );
 };
 
+const LEVEL_OPTIONS = Object.values(TLevel).map((n) => ({
+  label: n,
+  value: n,
+}));
+
 export function InvitationForm(props: {
   submit: (p: { users: string[]; level: TLevel }) => void;
   cancel: () => void;
 }) {
-  const [selection, setSelection] = useState<string[]>([]);
-  const [level, setLevel] = useState<TLevel | undefined>();
+  const methods = useForm<{ users: string[]; level: TLevel }>({
+    defaultValues: {},
+  });
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = methods;
+
+  console.log("errors", errors);
+
   return (
-    <div className="d-flex flex-column sub_container anim__rightToLeft mb-2">
-      <h4>Invite new users</h4>
-      <label>
-        Users to invite
-        <AsyncSelect
-          loadOptions={loadOptions as any} // FIXME TS
-          styles={customStyles}
-          cacheOptions
-          isMulti
-          noOptionsMessage={({ inputValue }) => {
-            return inputValue && inputValue.length > 0
-              ? "No user found for this search"
-              : "Start typing to search users";
-          }}
-          placeholder="Start typing to search users"
-          onChange={
-            (values) => setSelection([...values].map((e: any) => e.value)) // FIXME TS
-          }
-        />
-      </label>
-      <label className="mt-2">
-        Right level
-        <Select
-          styles={customStyles}
-          options={Object.values(TLevel).map((n) => ({ label: n, value: n }))}
-          onChange={(s) => setLevel(s?.value)}
-        />
-      </label>
-      <div className="d-flex justify-content-end">
-        <button
-          type="button"
-          className="btn btn-danger m-2"
-          onClick={() => props.cancel()}
-        >
-          Cancel
-        </button>
-        <button
-          disabled={!level || !selection}
-          className="btn btn-success m-2"
-          onClick={() =>
-            props.submit({ users: selection, level: level || TLevel.Read })
-          }
-        >
-          Invite users
-        </button>
-      </div>
-    </div>
+    <FormProvider {...methods}>
+      <form
+        onSubmit={handleSubmit((data) => props?.submit?.(data))}
+        className="d-flex flex-column sub_container anim__rightToLeft mb-2"
+      >
+        <h4>Invite new users</h4>
+        <label>
+          Users to invite*
+          <Controller
+            name="users"
+            control={control}
+            rules={{
+              required: "At least one user must be selected",
+            }}
+            render={({ field: { onChange, value } }) => (
+              <AsyncSelect
+                loadOptions={loadOptions as any} // FIXME TS
+                styles={customStyles}
+                cacheOptions
+                isMulti
+                noOptionsMessage={({ inputValue }) => {
+                  return inputValue && inputValue.length > 0
+                    ? "No user found for this search"
+                    : "Start typing to search users";
+                }}
+                placeholder="Start typing to search users"
+                onChange={(vs) => onChange(vs?.map((v) => v?.value))}
+                value={value ? { label: value, value: value } : []}
+              />
+            )}
+          />
+          <ErrorMessage errors={errors} name="users" />
+        </label>
+        <label className="mt-2">
+          Right level*
+          <Controller
+            name="level"
+            control={control}
+            rules={{
+              required: "Right level must be selected",
+            }}
+            render={({ field: { onChange, value } }) => (
+              <Select
+                value={LEVEL_OPTIONS.find((o) => o.value === value)}
+                onChange={(value) => {
+                  onChange(value?.value);
+                }}
+                styles={customStyles}
+                options={LEVEL_OPTIONS}
+              />
+            )}
+          />
+          <ErrorMessage errors={errors} name="level" />
+        </label>
+        <div className="d-flex justify-content-end">
+          <button
+            type="button"
+            className="btn btn-danger m-2"
+            onClick={() => props.cancel()}
+          >
+            Cancel
+          </button>
+          <button className="btn btn-success m-2" type="submit">
+            Invite users
+          </button>
+        </div>
+      </form>
+    </FormProvider>
   );
 }
