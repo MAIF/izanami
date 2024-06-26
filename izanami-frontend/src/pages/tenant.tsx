e12e60fa509d43693891aa4e77490a61bd900f9f
HelaKaraa <hela.karaa@gmail.com>
2024-06-26 17:57:04 +0200

    fix#769 : fix filter project

--- izanami-frontend/src/pages/tenant.tsx
+++ izanami-frontend/src/pages/tenant.tsx
@@ -1,7 +1,7 @@
 import { constraints, format, type } from "@maif/react-forms";
 import { Form } from "../components/Form";
 import * as React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useMutation, useQuery } from "react-query";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import queryClient from "../queryClient";
@@ -44,23 +44,23 @@ export function Tenant({ tenant }: { tenant: string }) {
 function ProjectList(props: { tenant: TenantType }) {
   const { tenant } = props;
   const location = useLocation();
-  let defaultProjects = location?.state?.item
-    ? [
-        {
-          id: location?.state?.item.id,
-          name: location?.state?.item.name,
-          description: location?.state?.item.description,
-        },
-      ]
-    : [];
   const queryKey = tenantQueryKey(tenant.name);
   const [creating, setCreating] = useState<boolean>(false);
   const [selectedProject, selectProject] = useState<
     TenantProjectType[] | undefined
-  >(defaultProjects);
+  >([]);
+  const [defaultProjectName, setDefaultProjectName] = useState("");
+
+  useEffect(() => {
+    if (location?.state?.item) {
+      const { id, name, description } = location.state.item;
+      selectProject([{ id, name, description }]);
+      setDefaultProjectName(name);
+    }
+  }, [location?.state?.item]);
 
   const { refreshUser } = React.useContext(IzanamiContext);
-
+  console.log("selectedProject", selectedProject);
   const projectCreationMutation = useMutation(
     (data: ProjectInCreationType) => createProject(tenant.name, data),
     {
@@ -73,7 +73,14 @@ function ProjectList(props: { tenant: TenantType }) {
 
   const hasTenantWriteRight = useTenantRight(tenant.name, TLevel.Write);
   const navigate = useNavigate();
-
+  const handleProjectSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchValue = e.target.value.toLowerCase();
+    const filteredProjects = tenant?.projects?.filter((project) =>
+      project.name.toLowerCase().startsWith(searchValue)
+    );
+    selectProject(filteredProjects);
+    setDefaultProjectName(searchValue);
+  };
   const noProjects = tenant?.projects?.length === 0;
 
   return (
@@ -92,15 +99,9 @@ function ProjectList(props: { tenant: TenantType }) {
       </div>
       <div className="d-flex flex-column">
         <input
-          defaultValue={location?.state?.item.name || ""}
+          value={defaultProjectName}
           placeholder="Search project"
-          onChange={(e) => {
-            selectProject(
-              tenant?.projects?.filter((f) =>
-                f.name.toLowerCase().startsWith(e.target.value.toLowerCase())
-              )
-            );
-          }}
+          onChange={handleProjectSearch}
           className="form-control"
           type="text"
           name="search-form-project"
