0e3e8eefc13f89301c9acfe43a476ffd008fc13f
Benjamin Cavy <benjamin.cavy@gmail.com>
2024-07-02 11:23:44 +0200

    WIP

--- app/fr/maif/izanami/datastores/ExportDatastore.scala
+++ app/fr/maif/izanami/datastores/ExportDatastore.scala
@@ -1,27 +1,21 @@
 package fr.maif.izanami.datastores
 
+import fr.maif.izanami.datastores.apiKeyImplicites.ApiKeyRow
 import fr.maif.izanami.datastores.featureImplicits.FeatureRow
+import fr.maif.izanami.datastores.projectImplicits.ProjectRow
 import fr.maif.izanami.datastores.tagImplicits.TagRow
 import fr.maif.izanami.env.Env
 import fr.maif.izanami.env.pgimplicits.EnhancedRow
 import fr.maif.izanami.errors.IzanamiError
-import fr.maif.izanami.models.{LightWeightFeature, Tag}
+import fr.maif.izanami.models.{ApiKey, LightWeightFeature, Project, Tag}
 import fr.maif.izanami.utils.Datastore
+import fr.maif.izanami.wasm.WasmConfig
 import fr.maif.izanami.web.ExportController.ExportRequest
 import play.api.libs.json.{JsArray, JsValue}
 
 import scala.concurrent.Future
 
 class ExportDatastore(val env: Env) extends Datastore {
-
-  /*def exportForTenant(tenant: String, exportRequest: ExportRequest): Future[Either[IzanamiError, JsArray]] = {
-    SELECT f.*, wsc.id as wasm_id,  json_arrayagg(ft.tag) as tags
-    FROM features f
-    LEFT OUTER JOIN wasm_script_configurations wsc ON f.script_config = wsc.id
-    LEFT OUTER JOIN features_tags ft ON ft.feature = f.id
-    GROUP by f.id, wsc.id
-  }*/
-
   def readFeatures(tenant: String): Future[(Set[LightWeightFeature], Set[String], Set[String])] = {
     env.postgresql
       .queryAll(
@@ -60,6 +54,38 @@ class ExportDatastore(val env: Env) extends Datastore {
       List(names.toArray),
       schemas = Set(tenant)
     ) { row => row.optTag() }
+  }
+
+  def readWasmConfigs(tenant: String, ids: Set[String]): Future[List[WasmConfig]] = {
+    env.postgresql.queryAll(s"""
+         |SELECT config
+         |FROM wasm_script_configurations
+         |WHERE id=ANY(
+         |""".stripMargin) { r => r.optJsObject("config").map(js => js.as(WasmConfig.format)) }
+  }
+
+  def readProjects(tenant: String, names: Set[String]): Future[List[Project]] = {
+    env.postgresql.queryAll(
+      s"""
+         |SELECT id, name, description
+         |FROM projects
+         |WHERE name=ANY($$1)
+         |""".stripMargin,
+      List(names.toArray),
+      schemas = Set(tenant)
+    ) { r => r.optProject() }
+  }
+
+  def readKeys(tenant: String, names: Set[String]): Future[List[ApiKey]] = {
+    env.postgresql.queryAll(
+      s"""
+         |SELECT *
+         |FROM apikeys
+         |WHERE name=ANY($$1)
+         |""".stripMargin,
+      List(names.toArray),
+      schemas = Set(tenant)
+    ) { r => r.optApiKey(tenant) }
   }
 
 }
