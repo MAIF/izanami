5b45f58b7244c5d5e430e5c9bf0d81c3a215f400
HelaKaraa <hela.karaa@gmail.com>
2024-10-31 17:25:54 +0100

    feat#857: add CustomOption for select filter

--- izanami-frontend/src/utils/searchUtils.tsx
+++ izanami-frontend/src/utils/searchUtils.tsx
@@ -11,28 +11,57 @@ export interface ISearchProps {
   allTenants?: string[];
   onClose: () => void;
 }
-export const SEARCH_TYPES_LABEL = {
-  TENANT: "Tenants",
-  PROJECT: "Projects",
-  FEATURE: "Features",
-  KEY: "Keys",
-  GLOBAL_CONTEXT: "Global Contexts",
-  LOCAL_CONTEXT: "Local Contexts",
-  TAG: "Tags",
-  WEBHOOK: "WebHooks",
-  SCRIPT: "WASM scripts",
-};
-export const SEARCH_TYPES_VALUE = {
-  TENANT: "tenant",
-  PROJECT: "project",
-  FEATURE: "feature",
-  KEY: "key",
-  GLOBAL_CONTEXT: "global_context",
-  LOCAL_CONTEXT: "local_context",
-  TAG: "tag",
-  WEBHOOK: "webhook",
-  SCRIPT: "script",
-};
+export const SearchTypeEnum = {
+  TENANT: {
+    value: "tenant",
+    label: "Tenants",
+    icon: <i className="fas fa-cloud me-2" aria-hidden="true" />,
+  },
+  PROJECT: {
+    value: "project",
+    label: "Projects",
+    icon: <i className="fas fa-building me-2" aria-hidden="true" />,
+  },
+  FEATURE: {
+    value: "feature",
+    label: "Features",
+    icon: <i className="fas fa-rocket me-2" aria-hidden="true" />,
+  },
+  KEY: {
+    value: "key",
+    label: "Keys",
+    icon: <i className="fas fa-key me-2" aria-hidden="true" />,
+  },
+  GLOBAL_CONTEXT: {
+    value: "global_context",
+    label: "Global Contexts",
+    icon: (
+      <span aria-hidden="true" className="me-2">
+        <GlobalContextIcon />
+      </span>
+    ),
+  },
+  LOCAL_CONTEXT: {
+    value: "local_context",
+    label: "Local Contexts",
+    icon: <i className="fas fa-filter me-2" aria-hidden="true" />,
+  },
+  TAG: {
+    value: "tag",
+    label: "Tags",
+    icon: <i className="fas fa-tag me-2" aria-hidden="true" />,
+  },
+  WEBHOOK: {
+    value: "webhook",
+    label: "WebHooks",
+    icon: <i className="fas fa-plug me-2" aria-hidden="true" />,
+  },
+  SCRIPT: {
+    value: "script",
+    label: "WASM scripts",
+    icon: <i className="fas fa-code me-2" aria-hidden="true" />,
+  },
+} as const;
 
 export type SearchModalStatus = { all: true } | { all: false; tenant: string };
 export type SearchResultStatus =
@@ -44,43 +73,43 @@ export type SearchResultStatus =
 export const getLinkPath = (item: SearchResult) => {
   const { tenant } = item;
   switch (item.type) {
-    case SEARCH_TYPES_VALUE.FEATURE: {
+    case SearchTypeEnum.FEATURE.value: {
       const projectName = item.path.find(
-        (p) => p.type === SEARCH_TYPES_VALUE.PROJECT
+        (p) => p.type === SearchTypeEnum.PROJECT.value
       )?.name;
       return `/tenants/${tenant}/projects/${projectName}?filter=${item.name}`;
     }
-    case SEARCH_TYPES_VALUE.PROJECT:
+    case SearchTypeEnum.PROJECT.value:
       return `/tenants/${tenant}/projects/${item.name}`;
-    case SEARCH_TYPES_VALUE.KEY:
+    case SearchTypeEnum.KEY.value:
       return `/tenants/${tenant}/keys?filter=${item.name}`;
-    case SEARCH_TYPES_VALUE.TAG:
+    case SearchTypeEnum.TAG.value:
       return `/tenants/${tenant}/tags/${item.name}`;
-    case SEARCH_TYPES_VALUE.SCRIPT:
+    case SearchTypeEnum.SCRIPT.value:
       return `/tenants/${tenant}/scripts?filter=${item.name}`;
-    case SEARCH_TYPES_VALUE.GLOBAL_CONTEXT: {
+    case SearchTypeEnum.GLOBAL_CONTEXT.value: {
       const maybeOpen = item.path
-        .filter((p) => p.type === SEARCH_TYPES_VALUE.GLOBAL_CONTEXT)
+        .filter((p) => p.type === SearchTypeEnum.GLOBAL_CONTEXT.value)
         .map((p) => p.name);
       const open = [...maybeOpen, item.name].join("/");
       return `/tenants/${tenant}/contexts?open=["${open}"]`;
     }
-    case SEARCH_TYPES_VALUE.LOCAL_CONTEXT: {
+    case SearchTypeEnum.LOCAL_CONTEXT.value: {
       const projectName = item.path.find(
-        (p) => p.type === SEARCH_TYPES_VALUE.PROJECT
+        (p) => p.type === SearchTypeEnum.PROJECT.value
       )?.name;
       const maybeOpen = item.path
         .filter(
           (p) =>
-            p.type === SEARCH_TYPES_VALUE.GLOBAL_CONTEXT ||
-            p.type === SEARCH_TYPES_VALUE.LOCAL_CONTEXT
+            p.type === SearchTypeEnum.GLOBAL_CONTEXT.value ||
+            p.type === SearchTypeEnum.LOCAL_CONTEXT.value
         )
         .map((p) => p.name);
       const open = [...maybeOpen, item.name].join("/");
 
       return `/tenants/${tenant}/projects/${projectName}/contexts?open=["${open}"]`;
     }
-    case SEARCH_TYPES_VALUE.WEBHOOK:
+    case SearchTypeEnum.WEBHOOK.value:
       return `/tenants/${tenant}/webhooks`;
   }
 };
@@ -91,89 +120,17 @@ export const typeDisplayInformation = new Map<
     icon: () => ReactElement<any, any>;
     displayName: string;
   }
->([
-  [
-    SEARCH_TYPES_VALUE.TENANT,
+>(
+  Object.values(SearchTypeEnum).map((type) => [
+    type.value,
     {
-      icon: () => <i className="fas fa-cloud me-2" aria-hidden="true" />,
-      displayName: SEARCH_TYPES_LABEL.TENANT,
+      icon: () => type.icon,
+      displayName: type.label,
     },
-  ],
-  [
-    SEARCH_TYPES_VALUE.FEATURE,
-    {
-      icon: () => <i className="fas fa-rocket me-2" aria-hidden="true" />,
-      displayName: SEARCH_TYPES_LABEL.FEATURE,
-    },
-  ],
-  [
-    SEARCH_TYPES_VALUE.PROJECT,
-    {
-      icon: () => <i className="fas fa-building me-2" aria-hidden="true" />,
-      displayName: SEARCH_TYPES_LABEL.PROJECT,
-    },
-  ],
-  [
-    SEARCH_TYPES_VALUE.KEY,
-    {
-      icon: () => <i className="fas fa-key me-2" aria-hidden="true" />,
-      displayName: SEARCH_TYPES_LABEL.KEY,
-    },
-  ],
-  [
-    SEARCH_TYPES_VALUE.TAG,
-    {
-      icon: () => <i className="fas fa-tag me-2" aria-hidden="true" />,
-      displayName: SEARCH_TYPES_LABEL.TAG,
-    },
-  ],
-  [
-    SEARCH_TYPES_VALUE.SCRIPT,
-    {
-      icon: () => <i className="fas fa-code me-2" aria-hidden="true" />,
-      displayName: SEARCH_TYPES_LABEL.SCRIPT,
-    },
-  ],
-  [
-    SEARCH_TYPES_VALUE.GLOBAL_CONTEXT,
-    {
-      icon: () => (
-        <span aria-hidden="true" className="me-2">
-          <GlobalContextIcon />
-        </span>
-      ),
-      displayName: SEARCH_TYPES_LABEL.GLOBAL_CONTEXT,
-    },
-  ],
-  [
-    SEARCH_TYPES_VALUE.LOCAL_CONTEXT,
-    {
-      icon: () => <i className="fas fa-filter me-2" aria-hidden="true" />,
-      displayName: SEARCH_TYPES_LABEL.LOCAL_CONTEXT,
-    },
-  ],
-  [
-    SEARCH_TYPES_VALUE.WEBHOOK,
-    {
-      icon: () => <i className="fas fa-plug me-2" aria-hidden="true" />,
-      displayName: SEARCH_TYPES_LABEL.WEBHOOK,
-    },
-  ],
-]);
-
-export const FilterOptions = [
-  { value: SEARCH_TYPES_VALUE.PROJECT, label: SEARCH_TYPES_LABEL.PROJECT },
-  { value: SEARCH_TYPES_VALUE.FEATURE, label: SEARCH_TYPES_LABEL.FEATURE },
-  { value: SEARCH_TYPES_VALUE.KEY, label: SEARCH_TYPES_LABEL.KEY },
-  {
-    value: SEARCH_TYPES_VALUE.GLOBAL_CONTEXT,
-    label: SEARCH_TYPES_LABEL.GLOBAL_CONTEXT,
-  },
-  {
-    value: SEARCH_TYPES_VALUE.LOCAL_CONTEXT,
-    label: SEARCH_TYPES_LABEL.LOCAL_CONTEXT,
-  },
-  { value: SEARCH_TYPES_VALUE.TAG, label: SEARCH_TYPES_LABEL.TAG },
-  { value: SEARCH_TYPES_VALUE.WEBHOOK, label: SEARCH_TYPES_LABEL.WEBHOOK },
-  { value: SEARCH_TYPES_VALUE.SCRIPT, label: SEARCH_TYPES_LABEL.SCRIPT },
-];
+  ])
+);
+export const FilterOptions = Object.values(SearchTypeEnum).map((type) => ({
+  value: type.value,
+  label: type.label,
+  icon: type.icon,
+}));
