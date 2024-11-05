a28c6e8373a6c7f404382fb5e86674e2ac798b1c
HelaKaraa <hela.karaa@gmail.com>
2024-11-05 14:42:54 +0100

    feat#857: Show Recent if the modalStatus is initial.

--- izanami-frontend/src/components/SearchComponant/SearchResults.tsx
+++ izanami-frontend/src/components/SearchComponant/SearchResults.tsx
@@ -59,10 +59,12 @@ export function SearchResults({
     (item) => modalStatus.all || item.tenant === modalStatus.tenant
   );
 
+  console.log("Result Status: ", resultStatus.state);
+
   return (
     <div className="search-container">
-      <div className="search-result">
-        {filteredHistory.length > 0 && (
+      {filteredHistory.length > 0 && resultStatus.state == "INITIAL" && (
+        <div className="search-result">
           <>
             <span>Recent</span>
             <ul className="search-ul nav flex-column">
@@ -72,82 +74,87 @@ export function SearchResults({
                     to={getLinkPath(term) || "#"}
                     onClick={() => handleItemClick(term)}
                   >
-                    <i className="fas fa-search me-2" aria-hidden="true" />
-                    {term.type}:{" "}
-                    {modalStatus.all
-                      ? `${term.tenant} / ${term.name}`
-                      : term.name}
+                    {typeDisplayInformation.get(term.type)?.icon() ?? ""}
+                    {term.name}
                   </Link>
                 </li>
               ))}
             </ul>
-            <hr />
           </>
-        )}
-        {resultStatus.state === "ERROR" ? (
-          <div>{resultStatus.error}</div>
-        ) : resultStatus.state === "PENDING" ? (
-          <div>Searching...</div>
-        ) : (
-          resultStatus.state === "SUCCESS" &&
-          (groupedItems?.size > 0 ? (
-            <ul className="search-ul nav flex-column">
-              {[...groupedItems.keys()].map((originTable, originTableIndex) => (
-                <li className="search-ul-item" key={originTable}>
-                  <span>
-                    {typeDisplayInformation.get(originTable)?.displayName ?? ""}
-                  </span>
-                  {groupedItems.get(originTable)?.map((item, index) => (
-                    <ol
-                      className="search-ul nav flex-column"
-                      key={`${item.name}-${originTableIndex}-${index}`}
-                    >
-                      <li
-                        className="search-ul-item"
-                        key={`${item.name}-${originTable}-${index}`}
-                      >
-                        <Link
-                          to={getLinkPath(item) || "#"}
-                          onClick={() => handleItemClick(item)}
-                        >
-                          <ul
-                            className="breadcrumb"
-                            style={{
-                              marginBottom: "0rem",
-                            }}
-                          >
-                            {item.path.map((pathElement, pathIndex) => (
-                              <li
-                                className="breadcrumb-item"
-                                key={`${item.name}-${pathIndex}`}
-                              >
-                                {typeDisplayInformation
-                                  .get(pathElement.type)
-                                  ?.icon() ?? ""}
-                                {pathElement.name}
-                              </li>
-                            ))}
-                            <li
-                              className="breadcrumb-item"
-                              key={`${item.name}-name`}
-                            >
-                              {typeDisplayInformation.get(item.type)?.icon() ??
-                                ""}
-                              {item.name}
-                            </li>
-                          </ul>
-                        </Link>
-                      </li>
-                    </ol>
-                  ))}
-                </li>
-              ))}
-            </ul>
+        </div>
+      )}
+
+      {resultStatus.state !== "INITIAL" && (
+        <div className="search-result">
+          {resultStatus.state === "ERROR" ? (
+            <div>{resultStatus.error}</div>
+          ) : resultStatus.state === "PENDING" ? (
+            <div>Searching...</div>
           ) : (
-            <div className="search-result">No results found</div>
-          ))
-        )}
-      </div>
+            resultStatus.state === "SUCCESS" &&
+            (groupedItems?.size > 0 ? (
+              <ul className="search-ul nav flex-column">
+                {[...groupedItems.keys()].map(
+                  (originTable, originTableIndex) => (
+                    <li className="search-ul-item" key={originTable}>
+                      <span>
+                        {typeDisplayInformation.get(originTable)?.displayName ??
+                          ""}
+                      </span>
+                      {groupedItems.get(originTable)?.map((item, index) => (
+                        <ol
+                          className="search-ul nav flex-column"
+                          key={`${item.name}-${originTableIndex}-${index}`}
+                        >
+                          <li
+                            className="search-ul-item"
+                            key={`${item.name}-${originTable}-${index}`}
+                          >
+                            <Link
+                              to={getLinkPath(item) || "#"}
+                              onClick={() => handleItemClick(item)}
+                            >
+                              <ul
+                                className="breadcrumb"
+                                style={{
+                                  marginBottom: "0rem",
+                                }}
+                              >
+                                {item.path.map((pathElement, pathIndex) => (
+                                  <li
+                                    className="breadcrumb-item"
+                                    key={`${item.name}-${pathIndex}`}
+                                  >
+                                    {typeDisplayInformation
+                                      .get(pathElement.type)
+                                      ?.icon() ?? ""}
+                                    {pathElement.name}
+                                  </li>
+                                ))}
+                                <li
+                                  className="breadcrumb-item"
+                                  key={`${item.name}-name`}
+                                >
+                                  {typeDisplayInformation
+                                    .get(item.type)
+                                    ?.icon() ?? ""}
+                                  {item.name}
+                                </li>
+                              </ul>
+                            </Link>
+                          </li>
+                        </ol>
+                      ))}
+                    </li>
+                  )
+                )}
+              </ul>
+            ) : (
+              <div className="search-result">No results found</div>
+            ))
+          )}
+        </div>
+      )}
     </div>
   );
 }
