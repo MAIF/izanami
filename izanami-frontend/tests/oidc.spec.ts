8507a6b304b04df068accf413798e0bb0afc02c1
Benjamin Cavy <benjamin.cavy@gmail.com>
2025-01-18 14:05:10 +0100

    fix: tests

--- izanami-frontend/tests/oidc.spec.ts
+++ izanami-frontend/tests/oidc.spec.ts
@@ -1,7 +1,7 @@
 import test, { expect } from "@playwright/test";
 
-test.describe("Home screen should", () => {
-  test("allow tenant creation", async ({ page }) => {
+test.describe("OIDC should", () => {
+  test("allow external login", async ({ page }) => {
     await page.goto("/login");
     await page.getByRole("button", { name: "OpenId connect" }).click();
     await expect(page).toHaveURL(
@@ -10,7 +10,7 @@ test.describe("Home screen should", () => {
     await page.getByLabel("Username").fill("User1");
     await page.getByLabel("Password").fill("pwd");
     await page.getByRole("button", { name: "Login" }).click();
-    await expect(page).toHaveURL("http://localhost:3000/home");
+    await expect(page).toHaveURL(/http:\/\/localhost:3000\/.*/);
     await expect(
       page.getByRole("button", { name: "Sam Tailor" })
     ).toBeVisible();
