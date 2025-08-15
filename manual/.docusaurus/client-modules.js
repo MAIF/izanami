541904b4e9c395e67129f1b2208219fec29f7995
Benjamin Cavy <benjamin.cavy@gmail.com>
2025-08-15 14:57:05 +0200

    doc: open feature doc for java client

--- manual/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/runner/work/izanami/izanami/manual/node_modules/infima/dist/css/default/default.css"),
-  require("/home/runner/work/izanami/izanami/manual/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/runner/work/izanami/izanami/manual/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/runner/work/izanami/izanami/manual/src/css/custom.css"),
-];
